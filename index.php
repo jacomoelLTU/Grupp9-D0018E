6575ecde657c9b29f0128d6a6b5f61e7f179f6c0
jacomoel <81552705+jacomoel@users.noreply.github.com>
2023-01-18 11:00:05 +0100

    first, download PHPserver in VS for live frontend

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html>
+        <body>
+
+            <h1>
+                <?php
+                echo "MINI blocket";
+                ?>
+
+            </h1>
+
+        </body>
+</html>
