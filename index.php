4ba48697b40c14b6ca8d92ce11d5401233ca67fd
wiklundemil <wiklund.emil2000@hotmail.com>
2023-01-20 14:23:32 +0100

    TESTAR

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
     <html>
         <body>
 
-            <h1>
+            
                 <?php
                 include 'config.php';
                 $namn = 'SELECT * FROM dummy WHERE name="Adam"';
                 echo $namn;
                 ?>
 
-            </h1>
+            
 
         </body>
 </html>
