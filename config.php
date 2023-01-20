74b278c7e669bcacb0f38513ec0e5cc6037a11d8
wiklundemil <wiklund.emil2000@hotmail.com>
2023-01-20 14:53:24 +0100

    TESTAR

--- config.php
+++ config.php
@@ -4,9 +4,12 @@ $username = "root";
 $password = "12345678";
 $db = "ecomercesite";
 // Create connection
+echo"1";
 $conn = mysqli_connect($servername, $username, $password,$db);
 // Check connection
+echo"2";
 if (!$conn) {
+  echo"3";
    die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
