b3a862022de57c8f528508bff4711318b4e9bcc0
wiklundemil <wiklund.emil2000@hotmail.com>
2023-01-22 15:55:02 +0100

    TESTAR

--- index.php
+++ index.php
@@ -5,9 +5,7 @@
                 echo "Här under borde det dyka upp ett namn:\n";
             
                 include 'config.php';
-                $query = $sql->prepare('SELECT * FROM user');
-                $query->execute();
-
+                $query = $sql('SELECT * FROM user');
                 $result = $query->fetchAll();
                 foreach($result as $r){
                     echo $r['user_id']; #Det vill inte printas ut... här de blir fel
