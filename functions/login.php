20a7fe3cd209ad53826c2ed01de0e1717ad523f1
wiklundemil <wiklund.emil2000@hotmail.com>
2023-01-25 20:07:13 +0100

    Fixing login

--- functions/login.php
+++ functions/login.php
@@ -13,12 +13,12 @@
         $query = mysqli_query($conn,"SELECT user_name, user_pwd FROM user WHERE user_name='$usrn' AND user_pwd='$pwd'");
         while($row=mysqli_fetch_array($query, MYSQLI_ASSOC)){
             if(($row['user_name'] == $usrn) && ($row['user_pwd'] == $pwd)){
-                $usrn    = $row['user_name'];
+                $usrn_db    = $row['user_name'];
                 $pwd_db  = $row['user_pwd'];
                 
                 session_start();
                 $_SESSION['username'] = $usrn_db;
-                $_SESSION['username'] = $pwd_db;
+                $_SESSION['loggedin'] = true;
             
             }
         }
