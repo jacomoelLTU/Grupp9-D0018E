480e3afb23770e3fc17debd0076c627dba05207d
Adam <adalju-0@student.ltu.se>
2023-01-25 14:11:14 +0100

    Added posts.css

--- forms/postForm.php
+++ forms/postForm.php
@@ -1,17 +1,26 @@
-<div id="postForm">
-    <!-- Form for post -->
-    <!-- Text Fields -->
-    <form action="../functions/posts.php" method="post">
-        Title: <input type="text" name="post_title">
-        Description: <input type="text" name="post_description">
-        Price: <input type="text" name="post_price">
-        <input type="submit" class = "post-button" name = "Submit" value="Post" />
-    </form>
-    
-    <!-- Image -->
-    <form action="../functions/upload.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="post_img" id="post_img">
-        <input type="submit" value="Upload Image" name="submit">
-    </form>
-</div>
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="CSS/posts.css">
+
+<h1>Make a Post</h1>
+
+<body>
+    <div id="postForm">
+        <!-- Form for post -->
+        <!-- Text Fields -->
+        <form action="../functions/posts.php" method="post">
+            Title: <input type="text" name="post_title">
+            Description: <input type="text" name="post_description">
+            Price: <input type="text" name="post_price">
+            <input type="submit" class = "post-button" name = "Submit" value="Post" />
+        </form>
+
+        <!-- Image -->
+        <form action="../functions/upload.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="post_img" id="post_img">
+            <input type="submit" value="Upload Image" name="submit">
+        </form>
+    </div>
+    </body>
+</html>
