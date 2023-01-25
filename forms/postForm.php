89edfec4659d8a1f57336d244b3ccddce36a11a7
Adam <adalju-0@student.ltu.se>
2023-01-25 14:58:46 +0100

    msg

--- forms/postForm.php
+++ forms/postForm.php
@@ -7,21 +7,25 @@
 
 <body>
     <div id="postForm">
+
         <!-- Form for post -->
         <!-- Text Fields -->
         <form action="../functions/posts.php" method="post">
             Title: <input type="text" name="post_title" placeholder="Enter title"><br> 
             Description: <br><textarea rows = "5" cols = "60" name = "post_description" placeholder="Enter description here..."></textarea><br>
             Price: <input type="text" name="post_price" placeholder="Enter price"><br>
-            <input type="submit" class = "post-button" name = "Submit" value="Upload Post"/><br>
-        </form>
 
-        <!-- Image -->
-        <form action="../functions/upload.php" method="post" enctype="multipart/form-data">
-            Select image to upload:
-            <input type="file" name="post_img" id="post_img">
-            <input type="submit" value="Upload Image" name="submit">
-        </form>
+            <!-- Image -->
+            <form action="../functions/upload.php" method="post" enctype="multipart/form-data">
+                Select image to upload:
+                <input type="file" name="post_img" id="post_img">
+                <input type="submit" value="Upload Image" name="submit">
+            </form>
+
+            <!-- Upload Button -->
+            <input type="submit" class = "post-button" name = "Submit" value="Upload Post"/><br>
+        </form> 
+        
     </div>
     </body>
 </html>
