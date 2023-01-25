0863a83c23ccb72a88e659f0787e862fe88d42d7
jacomoel <81552705+jacomoel@users.noreply.github.com>
2023-01-25 12:13:09 +0100

    fixed login link in signup

--- functions/signup.php
+++ functions/signup.php
@@ -15,7 +15,7 @@
 
                 <form class = "SignUpStyle" action ="registerUser.php" method = "post">
 
-                    <h1 class="SignUpHeader"> Provide user infromation to register </h1>
+                    <h1 class="SignUpHeader"> Provide user information to register </h1>
                     <input type = "text" class = "SignUp-input" name = "userName" placeholder = "Username"/> 
                     <input type = "text" class = "SignUp-input" name = "passWord" placeholder = "Password"/>
                     <input type = "text" class = "SignUp-input"  name = "userEmailAdress" placeholder = "Email Adress"/>
@@ -23,7 +23,7 @@
                     <input type = "text" class = "SinUp-input" name = "userSurname" placeholder = "Surname"/>
 
                     <input type="submit" class = "login-button" name = "Submit" value="Register" />
-                    <p class = "link"><a href="index.php">Click to view login page</a></p>
+                    <p class = "link"><a href="functions/index.php">Click to view login page</a></p>
 
                 </form>
     
