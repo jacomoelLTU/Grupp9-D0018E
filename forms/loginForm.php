b30ddcaf1a0a94fb1f936c7366625bf4902e2015
Adam <adalju-0@student.ltu.se>
2023-01-25 12:03:52 +0100

    Added postForm

--- forms/loginForm.php
+++ forms/loginForm.php
@@ -5,13 +5,3 @@
         <input type="submit">
     </form>
 </div>
-
-<!-- Form for post action -->
-<form action="../functions/posts.php" method="POST">
-    Title: <input type="text" name="post_title">
-    Description: <input type="text" name="post_description">
-    Price: <input type="text" name="post_price">
-    Image: <input type="image" name="post_img">
-  
-    <input type="submit">
-</form>
