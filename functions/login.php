5128dcb3f16277e0785c7ee3b28f5ce229a8bbc3
wiklundemil <wiklund.emil2000@hotmail.com>
2023-01-25 16:47:00 +0100

    Fixing login

--- functions/login.php
+++ functions/login.php
@@ -1,27 +1,17 @@
 <?php 
-include 'config.php';
-
-if (!isset($_POST['username'], $_POST['password']) ) {
-	exit('Fill in all the forms...');
-}
-if ($stmt = $con->prepare('SELECT user_name, user_password FROM user WHERE username = ?')) {
-	$stmt->bind_param('s', $_POST['username']);
-	$stmt->execute();
-	$stmt->store_result(); //store_result makes it possible to check if account exists in db
-   
-    if ($stmt->num_rows > 0) {
-        $stmt->bind_result($usrn_db, $pwd_db);
-        $stmt->fetch();
-        if (password_verify($_POST['password'], $pwd_db)) {
-            $_SESSION['user'] = $_POST['username'];
-            $_SESSION['loggedin'] = true;
-        } else {
-            echo 'Wrong credentials...';
-        }
-    } else {
-        echo 'Wrong credentials...';
+    include 'config.php';
+    //First we need to check that all values of the form is given.
+    if(!isset($_POST['username']) || !isset($_POST['password'])){
+        echo"Missing credentials...";
+    }
+    else{
+        $usrn = $_POST['username'];
+        $pwd  = $_POST['password'];
+        
+        $query = "SELECT * FROM user WHERE user_name=$usrn and 'user_pwd'=$pwd";
+        $stmt = $conn->query($query);
+        echo $usrn." with pwd:".$pwd;
     }
 
-}
-header('../index.php');
+    header('../index.php');
 ?>
