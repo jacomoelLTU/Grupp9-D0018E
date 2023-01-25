81efb3089b790c6bd78f481156851ec5d1aecc7a
jacomoel <81552705+jacomoel@users.noreply.github.com>
2023-01-25 12:03:39 +0100

    fixed signup placeholders

--- functions/signup.php
+++ functions/signup.php
@@ -16,11 +16,11 @@
                 <form class = "SignUpStyle" action ="registerUser.php" method = "post">
 
                     <h1 class="SignUpHeader"> Provide user infromation to register </h1>
-                    <input type = "text" class = "SignUp-input" name = "Username" placeholder = "userName"/> 
-                    <input type = "text" class = "SignUp-input" name = "Password" placeholder = "passWord"/>
-                    <input type = "text" class = "SignUp-input"  name = "Email Adress" placeholder = "userEmail"/>
-                    <input type = "text" class = "SignUp-input" name = "First name" placeholder = "userFirstname"/>
-                    <input type = "text" class = "SinUp-input" name = "Surname" placeholder = "userSurname"/>
+                    <input type = "text" class = "SignUp-input" name = "userName" placeholder = "Username"/> 
+                    <input type = "text" class = "SignUp-input" name = "passWord" placeholder = "Password"/>
+                    <input type = "text" class = "SignUp-input"  name = "userEmailAdress" placeholder = "Email Adress"/>
+                    <input type = "text" class = "SignUp-input" name = "userFirstnaem" placeholder = "First Name"/>
+                    <input type = "text" class = "SinUp-input" name = "userSurname" placeholder = "Surname"/>
 
                     <input type="submit" class = "login-button" name = "Submit" value="Register" />
                     <p class = "link"><a href="index.php">Click to view login page</a></p>
