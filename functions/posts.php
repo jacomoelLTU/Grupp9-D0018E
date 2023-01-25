f76dfda9d955a410c92b2f8c2f8ab87f08b25437
wiklundemil <wiklund.emil2000@hotmail.com>
2023-01-25 12:24:18 +0100

    Removed test code

--- functions/posts.php
+++ functions/posts.php
@@ -1,21 +1,3 @@
-<?php 
-$query = 'SELECT * FROM user';
-$result = $conn->query($query);
-if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-        echo "\n Printar en row: |".$row['user_id'].
-                                "|".$row['user_name'].
-                                "|".$row['user_firstname'].
-                                "|".$row['user_surname'].
-                                "|".$row['user_email'].
-                                "|";
-    }
-}
-else{
-    echo "Tablet är tomt.";
-}
-?>
-
 <!-- POST action to mysql -->
 <?php 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
