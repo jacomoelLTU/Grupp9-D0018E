92ed9f71d62f20e0eeff3f9ba161e8f385e3ba60
wiklundemil <wiklund.emil2000@hotmail.com>
2023-01-27 22:59:53 +0100

    Making site more visualy pleasing

--- index.php
+++ index.php
@@ -1,35 +1,33 @@
 <!DOCTYPE html>
     <html>
-    <?php include 'functions/config.php'; ?>
+    <?php include 'functions/config.php'; include 'functions/loginCheck.php' ?>
         
-        <head> 
+     <head> 
         <meta charset="UTF-8">
-        </head>
-
+    </head>    
         <body>
+
             <link rel="stylesheet" type="text/css" href="CSS/header.css">
             <div id="header">
+                
                 <ul id="menu">
-                <script src="JS/loginButton.js"></script>
-                <div class="wrapper">
-                    <button type="button" id="btn-1" class="ripple" data-ripple-color="#888">button</button>
-                </div>
+                    
                 </ul>
                 <ul id="login">
-                    <p><a href="forms/loginForm.php">Log in</a></p>
-                    <?php 
-                        session_start();
-                        if(isset($_SESSION['loggedin'])){
-                            echo"Inloggad som:". $_SESSION['username'];
-                        } 
-                    ?>
-                    <p><a href="../forms/signUpForm.html">Sign Up</a></p>
-                    <p><a href="../forms/postForm.php">Make a post!</a></p>
+                    <p>
+                    Click here to <a href="forms/loginForm.php">Log in</a>!
+                    Click here to <a href="../forms/signUpForm.html">Sign Up</a>!
+                    </p>
+                    <div id=""><a href="../forms/postForm.php">Make a post!</a></center>
                 </ul>
             </div>
+
+
             <link rel="stylesheet" type="text/css" href="CSS/middle.css">
             <div id="middle">
                 <?php include 'functions/posts.php'; ?> <!-- Döljer innehållet, bra för säkerhet -->
             </div>
+            
+        
         </body>
     </html>
