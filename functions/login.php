0fb965e3e3c6d6e2b6ace2eb15f6bd756cd224fd
wiklundemil <wiklund.emil2000@hotmail.com>
2023-01-27 13:22:58 +0100

    Fixing login

--- functions/login.php
+++ functions/login.php
@@ -8,17 +8,21 @@
     }
     else{
         $usrn = $_POST['username'];
-        $pwd  = $_POST['password'];
+        $pwd  = md5($_POST['password']);
         
         $query = mysqli_query($conn,"SELECT user_name, user_pwd FROM user WHERE user_name='$usrn' AND user_pwd='$pwd'");
         while($row=mysqli_fetch_array($query, MYSQLI_ASSOC)){
             if(($row['user_name'] == $usrn) && ($row['user_pwd'] == $pwd)){
-                $usrn_db    = $row['user_name'];
+                $usrn_db = $row['user_name'];
                 $pwd_db  = $row['user_pwd'];
                 
                 session_start();
                 $_SESSION['username'] = $usrn_db;
                 $_SESSION['loggedin'] = true;
+            }
+            else{
+                  header('Location:../forms/signupForm.php');
+
             }
         }
     }
