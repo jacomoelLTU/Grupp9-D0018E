6b862a6e3265d5c2363942692989bcbc56dd83dd
jacomoel <81552705+jacomoel@users.noreply.github.com>
2023-01-27 13:08:08 +0100

    more signup

--- functions/signup.php
+++ functions/signup.php
@@ -1,35 +1,6 @@
-<!DOCTYPE html>
-    <html>
-
-    <head> 
-        <title>Sign Up Form</title>
-    </head>
-
-    <?php include 'functions/config.php'; ?>
-
-        <body>
-
-        <!-- Create input fields according to DB table user, save in vars and then write it to BD -->
-            <link rel="stylesheet" type="text/css" href="CSS/header.css">
-            <div id="singUp_header">
-
-            <form class = "SignUpStyle" action ="registerUser.php" method = "post">
-
-                <h1 class="SignUpHeader"> Provide user information to register </h1>
-                <input type = "text" class = "SignUp-input" name = "userName" placeholder = "Username"/> 
-                <input type = "text" class = "SignUp-input" name = "passWord" placeholder = "Password"/>
-                <input type = "text" class = "SignUp-input"  name = "userEmailAdress" placeholder = "Email Adress"/>
-                <input type = "text" class = "SignUp-input" name = "userFirstname" placeholder = "First Name"/>
-                <input type = "text" class = "SinUp-input" name = "userSurname" placeholder = "Surname"/>
-
-                <input type="submit" class = "login-button" name = "Submit" value="Register" />
-                <p class = "link"><a href="loginForm.php">Click to view login page</a></p>
-
-            </form>
-
-                <?php
-                    //load in the config file to set up a connection to DB
-                    require ("../functions/config.php");
+    <?php 
+    
+    include 'functions/config.php';
 
                     //stripslashes removes backslashes, some protection agains injection i guess
                     $userName = stripslashes($_POST['userName']);
@@ -68,10 +39,3 @@
                         mysqli_query($conn, $sql_insertUser);
                     }
                  ?>
-
-            
-    
-
-            </div>
-        </body>
-    </html>
