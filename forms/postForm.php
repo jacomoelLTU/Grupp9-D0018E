7e97990ba261107b3c7023a30b19d94457b91e4d
Adam <adalju-0@student.ltu.se>
2023-01-31 20:27:06 +0100

    changes

--- forms/postForm.php
+++ forms/postForm.php
@@ -12,13 +12,13 @@ include '../functions/viewimage.php';
         <!-- Form for post -->
         <!-- Text Fields -->
         <form action="../functions/posts.php" method="post">
-            <p id="title"> Title: <input type="text" name="post_title" placeholder="Enter title" required></p> 
-            <p id="description"> Description: <br><textarea rows = "5" cols = "60" name = "post_description" placeholder="Enter description here..." required></textarea></p>
-            <p id="price"> Price: <input type="text" name="post_price" placeholder="Enter price" required></p>
+            <p id="title"> Title <input type="text" name="post_title" placeholder="Enter title" required></p> 
+            <p id="description"> Description <br><textarea rows = "5" cols = "60" name = "post_description" placeholder="Enter description here..." required></textarea></p>
+            <p id="price"> Price <input type="text" name="post_price" placeholder="Enter price" required></p>
             <input type="submit" class = "post_button" name = "Submit" value="Upload Post"/><br>
             <!-- Image -->
             <form action="../functions/upload.php" method="post" enctype="multipart/form-data">
-                Select image to upload:
+                Select image to upload
                 <input type="file" name="post_img" id="post_img">
             </form>
 
