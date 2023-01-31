ce958b729ad2732eb79c340b8e3cc6750f1e1cd9
Adam <adalju-0@student.ltu.se>
2023-01-31 16:04:29 +0100

    probablywrong

--- functions/upload.php
+++ functions/upload.php
@@ -1,6 +1,6 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["post_img"]["name"]);
+// $target_dir = "uploads/";
+$target_file = basename($_FILES["post_img"]["name"]);
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
@@ -12,6 +12,15 @@ if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
+
+        //
+        $image = $_FILES["post_img"]["tmp_name"]; 
+        $imgContent = addslashes(file_get_contents($image)); 
+         
+        // Insert image content into database 
+        $insert = $conn->query("INSERT into post (post_img) VALUES ('$imgContent', NOW())");
+        //
+
     } else {
         echo "File is not an image.";
         $uploadOk = 0;
