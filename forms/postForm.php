5e41ac961954f47eb6c014e336ba5a88958c82ae
Adam <adalju-0@student.ltu.se>
2023-01-31 20:19:52 +0100

    swapping to p

--- forms/postForm.php
+++ forms/postForm.php
@@ -12,9 +12,9 @@ include '../functions/viewimage.php';
         <!-- Form for post -->
         <!-- Text Fields -->
         <form action="../functions/posts.php" method="post">
-            Title: <input type="text" name="post_title" placeholder="Enter title" required><br> 
-            Description: <br><textarea rows = "5" cols = "60" name = "post_description" placeholder="Enter description here..." required></textarea><br>
-            Price: <input type="text" name="post_price" placeholder="Enter price" required><br>
+            <p id="title"> Title: <input type="text" name="post_title" placeholder="Enter title" required></p> 
+            <p id="description"> Description: <br><textarea rows = "5" cols = "60" name = "post_description" placeholder="Enter description here..." required></textarea></p>
+            <p id="price"> Price: <input type="text" name="post_price" placeholder="Enter price" required></p>
             <input type="submit" class = "post_button" name = "Submit" value="Upload Post"/><br>
             <!-- Image -->
             <form action="../functions/upload.php" method="post" enctype="multipart/form-data">
