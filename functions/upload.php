ae786ac64cf567172782f0f587109ae2c1d40bbb
Adam <adalju-0@student.ltu.se>
2023-01-31 15:53:33 +0100

    corrected error from before

--- functions/upload.php
+++ functions/upload.php
@@ -1,7 +1,6 @@
 <?php
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["post_img"]["name"]);
-$uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
