0f46fd48ff4673ee3b3ba24c93abea1f0b1ebdc0
Adam <adalju-0@student.ltu.se>
2023-01-31 21:08:17 +0100

    commit

--- forms/postForm.php
+++ forms/postForm.php
@@ -13,7 +13,7 @@ include '../functions/viewimage.php';
         <!-- Text Fields -->
         <form action="../functions/posts.php" method="post">
             <p id="title"> Title <input type="text" name="post_title" placeholder="Enter title" required></p> 
-            <p id="description"> Description <br><textarea rows = "5" cols = "60" name = "post_description" placeholder="Enter description here..." required></textarea></p>
+            <p id="description"> Description <br><textarea rows = "5" cols = "40" name = "post_description" placeholder="Enter description here..." required></textarea></p>
             <p id="price"> Price <input type="text" name="post_price" placeholder="Enter price" required></p>
             <input type="submit" class = "post_button" name = "Submit" value="Upload Post"/><br>
             <!-- Images -->
