4bfcca797c2f32417ae458b53f15c1ac6c5576a8
wiklundemil <wiklund.emil2000@hotmail.com>
2023-01-31 13:08:21 +0100

    Implementerade logout funktion, samt strukturerade om lite i html angående login/logout.

--- forms/loginCheck.php
+++ forms/loginCheck.php
@@ -2,9 +2,9 @@
 include '../functions/config';
 session_start();
 if(isset($_SESSION['loggedin'])){
-    echo"LOgged in as:".$_SESSION['username']." Click here to <a href='../functions/logout.php'>Logout</a>";
+    echo"Logged in as: ".$_SESSION['username'].", Click here to <a href='../functions/logout.php'>Logout</a>";
 }
-elseif(!isset($_SESSION['loggedin'])){
+if(!isset($_SESSION['loggedin'])){
     echo"Click here to <a href='../forms/loginForm.php'>Login</a>";
 }
 ?>
