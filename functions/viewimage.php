d840b763f70d9883f3e52287dbf568a86022d5dc
Adam <adalju-0@student.ltu.se>
2023-01-31 15:32:50 +0100

    sick

--- functions/viewimage.php
+++ functions/viewimage.php
@@ -5,11 +5,12 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include 'config.php';
 
-$result = $conn->query("SELECT post_img FROM post ORDER BY post_userid DESC"); 
+$result = $conn->query("SELECT post_img FROM post WHERE post_userid=6"); 
+// RDER BY post_userid DESC"
 ?>
 
 <div class="gallery"> 
     <?php while($row = $result->fetch_assoc()){ ?> 
-        <img src="data:image/jpg;charset=utf8;base64,<?php echo($row['post_img']); ?>" /> 
+        <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['post_img']); ?>" /> 
     <?php } ?> 
 </div> 
