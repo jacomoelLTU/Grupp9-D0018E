87c24ba3fdcd3627c4e4b3bec0c69cde904c9b4b
wiklundemil <wiklund.emil2000@hotmail.com>
2023-01-31 15:50:51 +0100

    Adding profilepage

---
+++ forms/userpage.php
@@ -0,0 +1,10 @@
+Click to go to <a href="../index.php">homepage</a>! 
+<div id="profileContainer">
+    <img src="../pictures/profilePictureTemplate.jpg">
+    <p>
+    This is <?php $_SESSION['username']?>'s page. 
+    </p>
+</div>
+
+<?php
+?>
