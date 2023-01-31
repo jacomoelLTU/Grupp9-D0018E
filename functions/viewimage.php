df6c76cea9fadc0a7f4b7c891dae65302dadb089
Adam <adalju-0@student.ltu.se>
2023-01-31 15:17:53 +0100

    sick

--- functions/viewimage.php
+++ functions/viewimage.php
@@ -4,13 +4,8 @@ require 'config.php';
 $result = $conn->query("SELECT post_image FROM post ORDER BY id DESC"); 
 ?>
 
-<?php if($result->num_rows > 0){ ?> 
-    <div class="gallery"> 
-        <?php while($row = $result->fetch_assoc()){ ?> 
-            <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['post_image']); ?>" /> 
-        <?php } ?> 
-    </div> 
-<?php }else{ ?> 
-    <p class="status error">Image(s) not found...</p> 
-<?php } ?>
-
+<div class="gallery"> 
+    <?php while($row = $result->fetch_assoc()){ ?> 
+        <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['post_image']); ?>" /> 
+    <?php } ?> 
+</div> 
