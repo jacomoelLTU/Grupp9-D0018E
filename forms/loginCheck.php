83904b150c5ad8b2228862a1a9acaddbc2ff24a2
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-02 11:11:08 +0100

    Adding profilepage

--- forms/loginCheck.php
+++ forms/loginCheck.php
@@ -2,6 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+session_start();
 if(!isset($_SESSION['loggedin'])){
     echo"Click here to <a href='../forms/loginForm.php'>Login</a>";
 }
