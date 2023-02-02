0e20f5a1dc2cd7630fa02ae21b7284855b5661a4
Adam <adalju-0@student.ltu.se>
2023-02-02 12:39:02 +0100

    gg

--- forms/postForm.php
+++ forms/postForm.php
@@ -8,6 +8,9 @@ include '../functions/viewimage.php';
 
     <link rel="stylesheet" type="text/css" href="../CSS/posts.css">
     <div class="posts">
+        <form action="../functions/viewimage.php";>
+            testing
+        </form>
         <!-- Form for post -->
         <!-- Text Fields -->
         <form action="../functions/posts.php" method="post">
--- functions/viewimage.php
+++ functions/viewimage.php
@@ -3,13 +3,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include 'config.php';
-
 $query = "SELECT post_img FROM post ORDER BY post_userid DESC";
 $result= mysqli_query($conn, $query);
-
 ?>
 
-<div class="gallery"> 
 <?php
      while($row = mysqli_fetch_assoc($result))
      {
@@ -17,5 +14,4 @@ $result= mysqli_query($conn, $query);
         echo $row['post_title']; 
      } 
 ?>
-</div> 
 
