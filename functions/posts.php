f6f5ac48772f75b3d747a6f43486a9b8aee1fdc9
Adam <adalju-0@student.ltu.se>
2023-02-02 16:14:46 +0100

    g

--- functions/posts.php
+++ functions/posts.php
@@ -1,5 +1,9 @@
 <!-- POST action to mysql -->
+
 <?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // include 'forms/postForm.php';
 require 'config.php';
 
