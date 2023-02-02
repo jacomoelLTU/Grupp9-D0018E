c3fe2d97881a8ca0883986579a1b29187fd17408
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-02 11:16:30 +0100

    Adding profilepage

--- forms/loginCheck.php
+++ forms/loginCheck.php
@@ -4,8 +4,9 @@ if(!isset($_SESSION['loggedin'])){
     echo"Click here to <a href='../forms/loginForm.php'>Login</a>";
 }
 else{
-   echo"Logged in as: ".$_SESSION['username'].", 
+   echo" 
     <nav id='userpage'>
+    Logged in as: ".$_SESSION['username'].",
         <div id='clickable'>    
             Click for <a href ='userpage.php' alt=''>Profile</a>
             and here to <a href='../functions/logout.php'>Logout</a>
