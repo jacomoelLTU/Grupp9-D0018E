d99bba1644f68ef226e7546582a4f2e5e739ef6a
Adam <adalju-0@student.ltu.se>
2023-02-02 13:04:11 +0100

    idek at this point

--- forms/postForm.php
+++ forms/postForm.php
@@ -1,7 +1,8 @@
 <?php 
 include '../functions/config.php';
 include '../functions/upload.php';
-include '../functions/viewimage.php';  
+include '../functions/viewimage.php';
+include '../functions/posts.php';  
 ?>
 <body>
     <h1>Make a Post</h1>
@@ -9,14 +10,15 @@ include '../functions/viewimage.php';
     <link rel="stylesheet" type="text/css" href="../CSS/posts.css">
     <div class="posts">
         <form action="../functions/viewimage.php">
+            Images should go here for testing
         </form>
-        <!-- Form for post -->
+        
         <!-- Text Fields -->
         <form action="../functions/posts.php" method="post">
             <p id="title"> Title <input type="text" name="post_title" placeholder="Enter title" required></p> 
             <p id="description"> Description <br><textarea rows = "5" cols = "40" name = "post_description" placeholder="Enter description here..." required></textarea></p>
-            <!-- <p id="price"> Price <input type="text" name="post_price" placeholder="Enter price" required></p> -->
             <input type="submit" class = "post_button" name = "Submit" value="Upload Post"/><br>
+            
             <!-- Images -->
             <div id="images"><form action="../functions/upload.php" method="post" enctype="multipart/form-data">
                 Select image to upload
