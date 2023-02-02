f5628e95631759dbea1b434d396581d1fd216776
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-02 11:10:14 +0100

    Adding profilepage

--- forms/loginCheck.php
+++ forms/loginCheck.php
@@ -3,11 +3,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 if(!isset($_SESSION['loggedin'])){
-    echo"Click here toooooo <a href='../forms/loginForm.php'>Login</a>";
+    echo"Click here to <a href='../forms/loginForm.php'>Login</a>";
 }
 else{
-    echo"Logged in as: ".$_SESSION['username'].", 
-    <nav id='userpage'>Click for <a href ='userpage.php' alt=''>Profile</a>
-    Click here to <a href='../functions/logout.php'>Logout</a>";
+    echo"VAFAN";
+  //  echo"Logged in as: ".$_SESSION['username'].", 
+   //  <nav id='userpage'>Click for <a href ='userpage.php' alt=''>Profile</a>
+   //  Click here to <a href='../functions/logout.php'>Logout</a>";
 }
 ?>
