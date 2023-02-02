044952a6bdd63b2e5fe9be0e3192eb9d4814c457
Adam <adalju-0@student.ltu.se>
2023-02-02 13:15:44 +0100

    g

--- functions/posts.php
+++ functions/posts.php
@@ -20,7 +20,7 @@ require 'config.php';
 
     //query
     
-    $sql = "INSERT INTO post (post_title, post_description, post_img) VALUES ('$post_title', '$post_description', '$post_img')";
+    $sql = "INSERT INTO post (post_userid, post_title, post_description, post_img) VALUES ('1', '$post_title', '$post_description', '$post_img')";
 
     //insert in mysql
     $rs = mysqli_query($conn, $sql);
