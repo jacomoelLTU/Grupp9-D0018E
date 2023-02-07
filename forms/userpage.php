b82dabcf8138cde75a9a3e87567a2329f36bb38c
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-07 15:41:08 +0100

    Fixing userpage

--- forms/userpage.php
+++ forms/userpage.php
@@ -2,7 +2,7 @@ Click to go to <a href="../index.php">homepage</a>!
 <div id="profileContainer">
     <img style="width:100px; height:100px;" src="../pictures/profilePictureTemplate.jpg">
     <p>
-    This is <?php session_start(); echo$_SESSION['username'];?>'s page. 
+    This is <?php session_start(); echo$_SESSION['username']."Med id: ".$_SESSION['userid'];?>'s page. 
     </p>
 </div>
 <?php
