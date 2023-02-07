19c4093dbb08a949cc2f8aa480d212622d8d30a8
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-07 15:45:09 +0100

    Fixing userpage

--- functions/userposts.php
+++ functions/userposts.php
@@ -1,8 +1,8 @@
 <?php
-include'config.php';
+include 'config.php';
 $userid = $_SESSION['userid'];
 
-$query = mysqli_query($conn, "SELECT * FROM post where post_userid:='$userid'");
+$query = mysqli_query($conn, "SELECT * FROM post WHERE post_userid='$userid'");
 
 while($row=mysqli_fetch_array($query, MYSQLI_ASSOC)){
     echo"TestLink is:"."?postId=".$row['post_id']."?postTitle".$row['post_title']."\n";
