1d71bc042ae48cb7516e3780c6ab96ad9d03c247
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-07 15:39:01 +0100

    Fixing userpage

--- functions/posts.php
+++ functions/posts.php
@@ -24,8 +24,7 @@ require 'config.php';
 
     //query
 session_start();
-$usrid = $_SESSION['username'];
-$uid = "SELECT user_id FROM user WHERE user_name='$usrid'";
+$uid = $_SESSION['userid'];
     $sql = "INSERT INTO post (post_userid, post_title, post_description, post_img) VALUES ('$uid', '$post_title', '$post_description', '$post_img')";
 
     //insert in mysql
