38bd3cdb6831dc13bc70b28d9c3fb69c648759c4
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-08 10:15:35 +0100

    Changed forms to pages, better def

--- functions/userposts.php
+++ functions/userposts.php
@@ -1,9 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include 'config.php';
 $userid = $_SESSION['userid'];
 
 $query = mysqli_query($conn, "SELECT * FROM post WHERE post_userid='$userid'");
 while($row=mysqli_fetch_array($query, MYSQLI_ASSOC)){
-    echo"Click to go to ".$row['post_title'].": <a href ='showpost.php?postId=".$row['post_id']."?postTitle=".$row['post_title']."'>Show post</a><br>";
+    echo"Click to go to post ".$row['post_title'].": <a href ='showpost.php?postId=".$row['post_id']."?postTitle=".$row['post_title']."'>Show post</a><br>";
 }
 ?>
