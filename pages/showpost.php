4a8ee71f0c8b9d437daf9bbf5b010165e1f86bf6
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-08 10:22:57 +0100

    Changed forms to pages, better def

--- pages/showpost.php
+++ pages/showpost.php
@@ -1,4 +1,4 @@
 <?php
-echo $_GET['postTitle'];
+echo "Should print title ".$_GET['postTitle'];
 //echo "Post with title[".htmlspecialchars($_GET["postTitle"])."] and id[".htmlspecialchars($_GET["postId"])."] is now shown!";
 ?>
