61ce115f1fb9c53bf21d49ed75160a9f3ee2c47b
Adam <adalju-0@student.ltu.se>
2023-02-08 12:59:40 +0100

    msg

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                 <div id="form-main-content">
                     <p>
                     Click here to <a href="../pages/postForm.php">Make a post</a>!
-                    Click here to <a href="../pages/postForm.php">view image</a>!
+                    Click here to <a href="../pages/printimage.php">view image</a>!
                     </p>
                 </div>
                 <center>Här är middle!</center>
