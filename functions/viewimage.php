5e076ddc0c5eb0063d54adcbe8d54161d9725b12
Adam <adalju-0@student.ltu.se>
2023-02-08 11:33:01 +0100

    trying to fix images

--- functions/viewimage.php
+++ functions/viewimage.php
@@ -3,15 +3,16 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include 'config.php';
-$query = "SELECT post_img FROM post ORDER BY post_userid DESC";
+$query = "SELECT post_title, post_img FROM post ORDER BY post_id DESC";
 $result = mysqli_query($conn, $query);
 ?>
 
 <?php
-    while($row = mysqli_fetch_assoc($result))
+    while($record = mysqli_fetch_assoc($result))
     {
     echo "this is typing";
-    echo '<h2>'.$row['post_img'].'</h2>';
+    echo '<h1>'.$record['post_title'].'</h1>';
+    echo '<h2>'.$record['post_img'].'</h2>';
     } 
 ?>
 
