591e4a2470a1f126a11c963631f399651e4cf8bd
Adam <adalju-0@student.ltu.se>
2023-02-08 11:50:21 +0100

    trying to fix images

--- pages/postForm.php
+++ pages/postForm.php
@@ -1,28 +1,8 @@
 <?php 
 include '../functions/config.php';
 include '../functions/upload.php';
-//include '../functions/viewimage.php';
- 
+include '../functions/viewimage.php';
 ?>
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-//include 'config.php';
-$query = "SELECT post_title, post_img FROM post ORDER BY post_id DESC";
-$result = mysqli_query($conn, $query);
-?>
-
-<?php
-    while($record = mysqli_fetch_assoc($result))
-    {
-    echo "this is typing";
-    echo '<h2>'.$record['post_title'].'</h1>';
-    echo '<img src="post/>'.$record['post_img'].'"</img>';
-    } 
-?>
-
-
 
 Click to go to <a href="../index.php">homepage</a>! 
 <body>
