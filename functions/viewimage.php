734fde94a6005b4a79455d980c54da5b6cd9806e
Adam <adalju-0@student.ltu.se>
2023-02-08 12:52:47 +0100

    msg

--- functions/viewimage.php
+++ functions/viewimage.php
@@ -10,6 +10,10 @@ $result = mysqli_query($conn, $query);
 <?php
     while($record = mysqli_fetch_assoc($result))
     {
+
+    $imageURL = 'uploads/'.$row["post_img"];
+    ?><img src="<?php echo $imageURL; ?>" alt="" /><?php
+
     echo "this is typing";
     echo "<h2>".$record['post_title']."</h1>";
     echo "<img src='images/>".$record['post_img']."'</img>";
