aba46e05f2c04e91e22e739ef94518c9b8c28e57
Adam <adalju-0@student.ltu.se>
2023-02-08 11:49:44 +0100

    trying to fix images

--- pages/postForm.php
+++ pages/postForm.php
@@ -8,7 +8,7 @@ include '../functions/upload.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include 'config.php';
+//include 'config.php';
 $query = "SELECT post_title, post_img FROM post ORDER BY post_id DESC";
 $result = mysqli_query($conn, $query);
 ?>
