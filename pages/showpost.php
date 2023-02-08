bda9e4d09f3aecbef794021983f6629451c1bc3a
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-08 16:23:14 +0100

    Added post site

--- pages/showpost.php
+++ pages/showpost.php
@@ -1,3 +1,16 @@
 <?php
-echo "Post with title[".htmlspecialchars($_GET["postTitle"])."] and id[".htmlspecialchars($_GET["postId"])."] is now shown!";
 ?>
+<link rel="stylesheet" type="text/css" href="../CSS/showpost.css">
+<center>
+<div id="postContainer">
+  <div id="grid-item">Post Title:       <?php $_GET['postTitle'];       ?></div>
+  <div id="grid-item">Post Description: <?php $_GET['postDescription']; ?></div>
+  <div id="grid-item">Post ...1</div>
+  <div id="grid-item">Post ...2</div>
+  <div id="grid-item">Post ...3</div>
+  <div id="grid-item">Post ...4</div>
+  <div id="grid-item">Post ...5</div>
+  <div id="grid-item">Post ...6</div>
+  <div id="grid-item">Post ...7</div>
+</div>
+</center>
