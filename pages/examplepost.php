ef7763bf5f6a2aebe24f630f40af3391a5b638e8
Adam <adalju-0@student.ltu.se>
2023-02-09 16:07:31 +0100

    gg

--- pages/examplepost.php
+++ pages/examplepost.php
@@ -1,9 +1,13 @@
 <link rel="stylesheet" type="text/css" href="../CSS/examplepost.css">
+
 Example how a post could look 
 <div id="preview">
-    <img src = "../pictures/isseymiyakebomber.jpg" width = "400px" height="auto"/><br>
+    <img src = "../pictures/isseymiyakebomber.jpg" width = "400px" height="auto"/>
+</div> 
+
+<div id="textdiv">
     Title: Issey Miyake Bomber <br>
     Description: Issey Miyake AW88/89 reversible <br>bomber jacka med print av Tomio Mohri.<br>Köpt på grailed. Skick 9/10. <br>Går för ca 13000kr.<br>
     Quantity: 1<br>
     Price: 12000kr 
-</div> 
+</div>
