a8068a9698097cf82e914152eb53ad500dadc54f
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-09 14:46:43 +0100

    fix showpost

--- pages/showpost.php
+++ pages/showpost.php
@@ -4,14 +4,14 @@
 <a href="../index.php">To home</a>
 <center>
 <div id="postContainer">
-  <div id="grid-item">Post Title:       <?php echo $_GET['postTitle'];       ?></div>
-  <div id="grid-item">Post Description: <?php echo $_GET['postDescription']; ?></div>
-  <div id="grid-item">Post ...1</div>
-  <div id="grid-item">Post ...2</div>
-  <div id="grid-item">Post ...3</div>
-  <div id="grid-item">Post ...4</div>
-  <div id="grid-item">Post ...5</div>
-  <div id="grid-item">Post ...6</div>
-  <div id="grid-item">Post ...7</div>
+  <div id="grid-Title">Post Title:       <?php echo $_GET['postTitle'];       ?></div>
+  <div id="grid-Desc">Post Description: <?php echo $_GET['postDescription']; ?></div>
+  <div id="grid-A">A</div>
+  <div id="grid-B">B</div>
+  <div id="grid-C">C</div>
+  <div id="grid-D">D</div>
+  <div id="grid-E">E</div>
+  <div id="grid-F">F</div>
+  <div id="grid-G">G</div>
 </div>
 </center>
