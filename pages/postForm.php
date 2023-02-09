97a5aa0b045e00a3cbcf733ae4b083c46f59280a
Adam <adalju-0@student.ltu.se>
2023-02-09 15:40:45 +0100

    gg

--- pages/postForm.php
+++ pages/postForm.php
@@ -29,7 +29,7 @@ Click to go to <a href="../index.php">homepage</a>!
             <div id="preview">
                 <img src = "../pictures/isseymiyakebomber.jpg" width = "400px" height="auto"/><br>
                 Title: Issey Miyake Bomber <br>
-                Description: Issey Miyake AW88/89 reversible staff bomber jacka med print av Tomio Mohri.<br>Köpt på grailed. Skick 9/10. <br>Går för ca 13000kr.<br>
+                Description: Issey Miyake AW88/89 reversible <br> bomber jacka med print av Tomio Mohri.<br>Köpt på grailed. Skick 9/10. <br>Går för ca 13000kr.<br>
                 Quantity: 1<br>
                 Price: 12000kr 
         </div> 
