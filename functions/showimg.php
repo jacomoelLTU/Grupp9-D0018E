f801da270322a0610a06d64fc08cee15f049d24c
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-09 10:47:21 +0100

    try to fix img

---
+++ functions/showimg.php
@@ -0,0 +1,20 @@
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// Include the database configuration file  
+include 'config.php'; 
+ 
+// Get image data from database 
+$result = mysqli_query($conn,"SELECT post_img FROM post ORDER BY id DESC"); 
+?>
+
+<?php if($result->num_rows > 0){ ?> 
+    <div class="gallery"> 
+        <?php while($row = $result->fetch_assoc()){ ?> 
+            <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image']); ?>" /> 
+        <?php } ?> 
+    </div> 
+<?php }else{ ?> 
+    <p class="status error">Image(s) not found...</p> 
+<?php } ?>
