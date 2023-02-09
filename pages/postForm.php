55fe02dc4d85619f5bb87447e6eb7bc5bb4634d8
Adam <adalju-0@student.ltu.se>
2023-02-09 15:23:17 +0100

    gg

--- pages/postForm.php
+++ pages/postForm.php
@@ -27,7 +27,9 @@ Click to go to <a href="../index.php">homepage</a>!
             </form>
             Example how a post could look <br>
             <div id="preview">
-            <div id ="isseymiyake" img src = "../pictures/isseymiyakebomber.jpg"/>
+                <div id="isseymiyake">
+                <img src = "../pictures/isseymiyakebomber.jpg"/>
+                </div>
             Title: Issey Miyake Bomber <br>
             Description: Issey Miyake AW88/89 reversible staff bomber jacka med print av Tomio Mohri. Köpt på grailed. Skick 9/10. Går för ca 13000kr.<br>
             Quantity: 1<br>
