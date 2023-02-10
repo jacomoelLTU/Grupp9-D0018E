b15a4a8164f2ac751f7aabb2b18d78bed1aa5205
jacomoel <81552705+jacomoel@users.noreply.github.com>
2023-02-10 11:56:41 +0100

    work jao

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
     <?php include 'functions/config.php';?>
      <head> 
         <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="CSS/middle.css">
+        
     </head>    
         <body>
-            <!-- <link rel="stylesheet" type="text/css" href="CSS/header.css"> -->
+            <link rel="stylesheet" type="text/css" href="CSS/header.css">
             <div id="header">
                 <?php 
                 include 'pages/loginCheck.php';
@@ -19,7 +19,7 @@
             </div>
 
 
-            <!-- <link rel="stylesheet" type="text/css" href="CSS/middle.css"> -->
+            <link rel="stylesheet" type="text/css" href="CSS/middle.css">
             <div id="middle">
                 <div id="form-main-content">
                     <p>
@@ -27,12 +27,13 @@
                     Click here to <a href="../pages/printimage.php">view image</a>!
                     Click here for <a href="../pages/examplepost.php">post example</a>!
                     </p>
+
                 </div>
                 <center>Här är middle!</center>
             </div>
 
-        
-        <div  id = "cart" type = "cart" name = "cartButton">
+        <link rel="stylesheet" type="text/css" href="CSS/middle.css">
+        <div  id = "cartIcon_css" type = "cart" name = "cartButton">
             <a href = "pages/cartpage.php"><img src = "pictures/cartIcon.png"/>
             <?php
                 echo 'cart item count';
