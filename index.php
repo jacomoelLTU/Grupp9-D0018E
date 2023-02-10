e27a5783ef0a2cb85837d51d31b751c1d1d38166
jacomoel <81552705+jacomoel@users.noreply.github.com>
2023-02-10 12:53:10 +0100

    dunno

--- index.php
+++ index.php
@@ -32,9 +32,11 @@
                 <center>Här är middle!</center>
             </div>
 
-        <link rel="stylesheet" type="text/css" href="CSS/middle.css">
-        <div  id = "cartIcon"><!-- parent div to cartIcon -->
-            <a href = "pages/cartpage.php"><img src = "pictures/cartIcon.png" width="40" height="40"/><?php echo 'cart item count';?></a>
+        
+        <div  id = "cartIcon">
+            <a href = "pages/cartpage.php"><img src = "pictures/cartIcon.png" width="40" height="40"/></a>
+             <?php //echo 'cart item count';?>
+             <p>cart item count</p>
         </div>
             
         
