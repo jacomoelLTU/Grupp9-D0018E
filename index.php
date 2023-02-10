3a1a4edb81aec1c6e2f5ad925df299b584a8d93c
jacomoel <81552705+jacomoel@users.noreply.github.com>
2023-02-10 12:41:34 +0100

    scale inline

--- index.php
+++ index.php
@@ -34,10 +34,12 @@
 
         <link rel="stylesheet" type="text/css" href="CSS/middle.css">
         <div  id = "cartIcon"><!-- parent div to cartIcon -->
-            <a href = "pages/cartpage.php"><img src = "pictures/cartIcon.png"/></a>
-            <?php
+            <a href = "pages/cartpage.php"><img src = "pictures/cartIcon.png" width="40" height="40"/></a>
+           
+           <?php
                 echo 'cart item count';
             ?>
+
         </div>
             
         
