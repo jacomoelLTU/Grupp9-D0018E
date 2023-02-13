122236153d044b74eca3ee0135e90d6506894441
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-13 09:39:11 +0100

    Fixing view post

--- pages/showpost.php
+++ pages/showpost.php
@@ -7,9 +7,11 @@
   <div id="grid-Title">Post Title:       <?php echo $_GET['postTitle'];       ?></div>
   <div id="grid-Desc">Post Description: <?php echo $_GET['postDescription']; ?></div>
   <div id="grid-Image">
-    <img src="../pictures/profilePictureTemplate.jpg">
-    <img src="../pictures/img2.png">
-    
+    <!-- För att lägga till alla bilder som är uppladdade på posten kan vi ha som whileloopen i php som printar ut img strängarna med rätt värden. -->
+    <div id="img-container">
+      <img src="../pictures/profilePictureTemplate.jpg" class="img1">
+      <img src="../pictures/img2.png" class="img2">
+    </div>
   </div>
   <div id="grid-B">B</div>
   <div id="grid-C">C</div>
