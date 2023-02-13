4364a0b106b69ee0ce426172100f9376d399afbc
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-13 09:07:20 +0100

    Fixing view post

--- pages/showpost.php
+++ pages/showpost.php
@@ -7,7 +7,6 @@
   <div id="grid-Title">Post Title:       <?php echo $_GET['postTitle'];       ?></div>
   <div id="grid-Desc">Post Description: <?php echo $_GET['postDescription']; ?></div>
   <div id="grid-Imgage">
-    <img src="../pictures/profilePictureTemplate.jpg">
   </div>
   <div id="grid-B">B</div>
   <div id="grid-C">C</div>
