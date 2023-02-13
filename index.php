2ef841616bc903973512a92d65d9a54a2e1cc14d
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-13 13:26:24 +0100

    Fixing view post

--- index.php
+++ index.php
@@ -12,12 +12,11 @@
                 include 'pages/loginCheck.php';
                 ?>
                 <ul id="login"> 
+                    
                     <p>
                     Click here to <a href="../pages/signUpForm.php" >Sign Up</a>!
                     </p>
-                </ul>
-
-                <link rel="stylesheet" type="text/css" href="CSS/middle.css">
+                    <link rel="stylesheet" type="text/css" href="CSS/middle.css">
                 <div id = "cartIcon">
                     <!-- Added php so that it can change the url to current cartURL session -->
                     <?php
@@ -25,8 +24,9 @@
                         echo '<a id = "cartIconLink" href = "pages/cartpage.php'.$_SESSION["cartURL"].'"><img src = "pictures/cartIcon.png" width="40" height="40"/></a>';
                     ?>
                     <p> <?php echo 'cart items var'?> <p>        
-                </div>  
-                
+                </div> 
+                </ul> 
+
             </div>
          
             <link rel="stylesheet" type="text/css" href="CSS/middle.css">
