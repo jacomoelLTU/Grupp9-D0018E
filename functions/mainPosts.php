8f5b486bd92b37a9a2c3d4d99b61662a7f52aa44
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-13 11:44:20 +0100

    Fixing view post

--- functions/mainPosts.php
+++ functions/mainPosts.php
@@ -6,10 +6,10 @@ include 'config.php';
 $query = mysqli_query($conn, "SELECT * FROM post");
 while($row=mysqli_fetch_array($query, MYSQLI_ASSOC)){
    if($row['post_type'] == "informative"){
-    $type = "<h3 style='color:lightgreen;'>[".$row['post_type']."]</h3>"; 
+    $type = "<p style='color:lightgreen;'>[".$row['post_type']."]</p>"; 
    }
    else{
-        $type = "<h3 style='color:lightseeblue;'>[".$row['post_type']."]</h3>"; 
+        $type = "<p style='color:lightseeblue;'>[".$row['post_type']."]</p>"; 
    }
     echo $type." Click for post: ".$row['post_title'].": <a href ='showpost.php?postId=".$row['post_id']."&postTitle=".$row['post_title']."&postDescription=".$row['post_description']."'>Show post</a><br>";
 }
