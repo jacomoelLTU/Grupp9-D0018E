34976c05704a2a7ac41a7302f02c628c6076d338
jacomoel <81552705+jacomoel@users.noreply.github.com>
2023-02-13 11:47:30 +0100

    started cartpage

--- pages/cartpage.php
+++ pages/cartpage.php
@@ -1,8 +1,21 @@
-<div id="cartform">
-<a href="../index.php">To home</a>
 
-    <?php
-    echo 'This is your cart'
-    ?>
+<link rel="stylesheet" type="text/css" href="../CSS/cartpage.css">
 
+<div id = "cartHeader">
+   <h1 id = "cartWelcome"> Welcome to your shoppingcart!</h1> 
+    <p id = "cartBackToHome">Click the link to got to home page, <a href="../index.php">To home</a></p>
 </div>
+
+<body>
+
+    <!-- ROOM FO THE LIST OF ALL PRODUCTS IN THE CART -->
+
+    <!-- this two buttons should display after rows with items in the cart -->
+    <div id = "buttons">
+        <button type = "button" onclick="alert('Continue to card payment (outside of the course goals)')" id="checkoutButton" >PayNow</button>
+
+        <!-- below should be be the button that sets of rollBack in tables when a purchase gets canled -->
+        <button type="button" id="cancelButton" value="Cancel">Cancel purchase</button>
+    </div>
+
+</body>
