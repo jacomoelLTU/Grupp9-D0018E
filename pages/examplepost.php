01188b279fcb1206519d864dbf3aa83314eb66f9
Adam <adalju-0@student.ltu.se>
2023-02-15 11:45:55 +0100

    a lil bit of testing

--- pages/examplepost.php
+++ pages/examplepost.php
@@ -3,24 +3,25 @@
 Example how a post could look 
 <div id="preview">
     <img src = "../pictures/isseymiyakebomber.jpg" width = "400px" height="auto"/>
-</div> 
 
-<div id = "addToCartParentDiv">
-    <button id = "addtoCartButton" value = "Add to cart"></button>
-</div>
 
-<div id="textdiv">
-    Title: Issey Miyake Bomber <br>
-    Description: Issey Miyake AW88/89 reversible <br>bomber jacka med print av Tomio Mohri.<br>Köpt på grailed. Skick 9/10. <br>Går för ca 13000kr.<br>
-    Quantity: 1<br>
-    Price: 12000kr 
-</div>
-
-<body onload="getRating('../functions./getRating.php')">
-    <div class="container">
-        <h2>Rating System</h2>
+    <div id = "addToCartParentDiv">
+        <button id = "addtoCartButton" value = "Add to cart"></button>
     </div>
-</body>
+
+    <div id="textdiv">
+        Title: Issey Miyake Bomber <br>
+        Description: Issey Miyake AW88/89 reversible <br>bomber jacka med print av Tomio Mohri.<br>Köpt på grailed. Skick 9/10. <br>Går för ca 13000kr.<br>
+        Quantity: 1<br>
+        Price: 12000kr 
+    </div>
+
+    <body onload="getRating('../functions./getRating.php')">
+        <div class="container">
+            <h2>Rating System</h2>
+        </div>
+    </body>
+</div> 
 
 <!-- Har detta script här medans jag testar -->
 <script>
