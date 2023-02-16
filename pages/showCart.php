c01722546583667678ee9a10f355c6679d77d073
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-16 16:24:44 +0100

    Fixing rollbacks

--- pages/showCart.php
+++ pages/showCart.php
@@ -1,20 +1,21 @@
 <?php
 session_start();
 echo"Here is you items:";
-
+$delCount = 0;
 
     foreach($_SESSION['objArr'] as $r){
         $dataArr   = parse_str($r, $output);
         $postId    = $output['postId'];
         $postTitle = $output['postTitle']; 
         
-        echo "<div id='cartItem'>".$r." ID of post: ".$postId.
+        echo "<div id='cartItem'>".$r.
                 "<form method='post'>
                     <input type='submit' name='delObj' class='button' value='Del Item'/>
                 </form>
               </div><br>";   
     }
-    if(array_key_exists('delObj', $_POST)) {
+    if(array_key_exists('delObj', $_POST) && $delCount == 0) {
+        $delCount .=1;
         delObj($r, $postId);
     }
 
@@ -23,9 +24,10 @@ echo"Here is you items:";
     function delObj($r, $p) {
         session_start();
         $i = array_search($r, $_SESSION['objArr']);
-        if(parse_str($_SESSION['arrObj'[$i]],$p) == $GLOBALS['postId']){
+        if($_SESSION['objArr'][$i] == $r){            
             unset($_SESSION['objArr'][$i]);
+            header("Refresh:0");
         }
-        header("Location:cartpage.php");
-    }   
+    
+    }
 ?>
