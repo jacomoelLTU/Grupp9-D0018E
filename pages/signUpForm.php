deffaf27719991cbe5f060f418a8e1de4de78062
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-16 09:36:49 +0100

    Fixing mainPost

--- pages/signUpForm.php
+++ pages/signUpForm.php
@@ -1,14 +1,28 @@
-<form class = "SignUpStyle" action ="../functions/signup.php" method = "post">
+<?php
+    $usr = $_SESSION['username'];
+    $sql = "SELECT user_role, user_name FROM user WHERE user_name = '$usr'";
+    $query = mysqli_query($conn, $sql); 
+    while($row=mysqli_fetch_array($query, MYSQLI_ASSOC)){
+        if(($row['user_name'] == $usr)){
+            $role = $row['user_role'];
+        }
+    }
+    if($role =='admin'){
+    echo'
+        <form class = "SignUpStyle" action ="../functions/signup.php" method = "post">
 
-    <h1 class ="SignUpHeader"> Provide user information to register </h1>
-    <input type = "text" class = "SignUp-input" name = "userName" placeholder = "Username"/> 
-    <input type = "text" class = "SignUp-input" name = "passWord" placeholder = "Password"/>
-    <input type = "text" class = "SignUp-input"  name = "userEmailAdress" placeholder = "Email Adress"/>
-    <input type = "text" class = "SignUp-input" name = "userFirstname" placeholder = "First Name"/>
-    <input type = "text" class = "SinUp-input" name = "userSurname" placeholder = "Surname"/>
+            <h1 class ="SignUpHeader"> Provide user information to register </h1>
+            <input type = "text" class = "SignUp-input" name = "userName" placeholder = "Username"/> 
+            <input type = "text" class = "SignUp-input" name = "passWord" placeholder = "Password"/>
+            <input type = "text" class = "SignUp-input"  name = "userEmailAdress" placeholder = "Email Adress"/>
+            <input type = "text" class = "SignUp-input" name = "userFirstname" placeholder = "First Name"/>
+            <input type = "text" class = "SinUp-input" name = "userSurname" placeholder = "Surname"/>
 
-    <input type="submit" class = "login-button" name = "Submit" value="Register" />
-    
-    <p class = "link"><a href="loginForm.php">Click to view login page</a></p>
+            <input type="submit" class = "login-button" name = "Submit" value="Register" />
+            
+            <p class = "link"><a href="loginForm.php">Click to view login page</a></p>
 
-</form>
+        </form>
+    ';
+ }
+?>
