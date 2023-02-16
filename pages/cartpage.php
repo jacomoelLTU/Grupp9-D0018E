86fd1cf3d19af84aaa952f32f32531732b450eb1
jacomoel <81552705+jacomoel@users.noreply.github.com>
2023-02-16 14:28:03 +0100

    fixing write to product table

--- pages/cartpage.php
+++ pages/cartpage.php
@@ -1,6 +1,10 @@
 
-<link rel="stylesheet" type="text/css" href="../CSS/cartpage.css">
+<?php
+include "../functions/config.php";
 
+?>
+
+<link rel="stylesheet" type="text/css" href="../CSS/cartpage.css">
 <div id = "cartHeader">
    <h1 id = "cartWelcome"> Welcome to your shoppingcart!</h1> 
     <p id = "cartBackToHome">Click the link to got to home page, <a href="../index.php">To home</a></p>
@@ -9,12 +13,17 @@
 <body>
 
     <!-- ROOM FO THE LIST OF ALL PRODUCTS IN THE CART -->
+    <!-- description with seller user name , a list (1 - n) -->
+    <!-- price summary -->
+    <?php
+        
+    ?>
 
     <!-- this two buttons should display after rows with items in the cart -->
     <div id = "buttons">
         <button type = "button" onclick="alert('Continue to card payment (outside of the course goals)')" id="checkoutButton" >PayNow</button>
 
-        <!-- below should be be the button that sets of rollBack in tables when a purchase gets canled -->
+        <!-- Cancle button, initiates rollback of tables -->
         <button type="button" id="cancelButton" value="Cancel">Cancel purchase</button>
     </div>
 
