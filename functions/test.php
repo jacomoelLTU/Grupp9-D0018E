3fcc18767a15938a9da6dceee2abc362fc1a519f
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-17 21:52:21 +0100

    Fixing rollback

--- functions/test.php
+++ functions/test.php
@@ -1,9 +1,17 @@
 <link rel="stylesheet" type="text/css" href="CSS/middle.css">
+
+<form method="post">
+  <input type="submit" name="delItems" class="button" value="Del Items"/>
+  <input type="submit" name="rollback" class="button" value="RollbacK!"/>
+</form>
+
 <?php
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
   include 'config.php';
+  mysqli_autocommit($conn, FALSE);
+
   $query = mysqli_query($conn, "SELECT product_id, product_title FROM product");
   while($row=mysqli_fetch_array($query, MYSQLI_ASSOC)){
   echo "<center>
@@ -12,23 +20,18 @@
             </div>
       </center>";
   }
-?>
+  mysqli_commit($conn);
 
-<form method="post">
-      <input type="submit" name="delItem" class="button" value="Del Item"/>
-    </form>
-<?php
-    if(array_key_exists('delItem', $_POST)) {
-        delItem();
+   if(array_key_exists('delItems', $_POST)) {
+        delItems($conn);
     }
-  function delItem() {
-   echo"clicked the button!!!!!!!!!!!!!!!!";
-    // session_start();
-    // if(!isset($objArr)){
-    //   $_SESSION['objArr'] = array();
-    // }
-    // echo "Current object".$GLOBALS['object'];
-    // array_push($_SESSION['objArr'], $GLOBALS['object']); //Adds a new object to 'cart'
+    if(array_key_exists('rollback', $_POST)) {
+      rollback($conn);
   }
-  
+  function delItems($conn) {
+    mysqli_query($conn, "DELETE FROM product WHERE product_id='10'");
+  }
+  function rollback($conn) {
+    mysqli_rollback($conn);
+  }
 ?>
