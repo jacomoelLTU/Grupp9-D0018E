8d01b8033188dc11d11bbcbae1be0fe3ae2db976
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-23 15:18:28 +0100

    Fixing rollback

--- pages/showPost.php
+++ pages/showPost.php
@@ -70,9 +70,8 @@ error_reporting(E_ALL);
         if((mysqli_num_rows($query) < 0)){
           mysqli_query($conn, "INSERT INTO `transaction`(transaction_userid) VALUES($usrid)"); 
         }
-        else{
+        elseif((mysqli_num_rows($query) < 0) && $row=mysqli_fetch_array($query, MYSQLI_ASSOC)){
           $query = mysqli_query($conn, "SELECT transaction_id, transaction_userid FROM `transaction` WHERE transaction_userid=$usrid AND transaction_state='ongoing'");
-          $row=mysqli_fetch_array($query, MYSQLI_ASSOC);
          
           $ongoing_transaction_id           = $row['transaction_id'];
           $_SESSION['ongoingtransactionid'] = $row['transaction_id'];
