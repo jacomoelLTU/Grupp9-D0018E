b35a3bcd15773b773551f74c3ede9055270621d0
Adam <adalju-0@student.ltu.se>
2023-02-24 12:34:14 +0100

    if post_img has value

--- pages/printImage.php
+++ pages/printImage.php
@@ -9,16 +9,20 @@
     $result = mysqli_query($conn, $query);
 
     while($row=mysqli_fetch_array($result, MYSQLI_ASSOC)){
+        
+        //if post_img has value
+        if (isset($row['post_img'])) {
 
-        //get url from post_img
-        $url = "$row[post_img]";
+            //get url from post_img
+            $url = "$row[post_img]";
 
-        //get content from the url and encode so we can see the image
-        $image = base64_encode(file_get_contents($url));
+            //get content from the url and encode so we can see the image
+            $image = base64_encode(file_get_contents($url));
 
-        //print title and image
-        echo $row['post_title'];
-        echo '<img src="data:image/jpeg;base64,'.$image.'">';
+            //print title and image
+            echo $row['post_title'];
+            echo '<img src="data:image/jpeg;base64,'.$image.'">';
+        }
     }
     ?>
 </div>
