ecc99a8ed4500cf2e72b767f2e4689c5d161dc99
Adam <adalju-0@student.ltu.se>
2023-02-24 11:30:15 +0100

    trying to print image with url instead

--- pages/postForm.php
+++ pages/postForm.php
@@ -16,16 +16,17 @@ Click to go to <a href="../index.php">homepage</a>!
             <p id="description"> Description <br><textarea rows = "5" cols = "40" name = "post_description" placeholder="Enter description here..." required></textarea></p>
             <p id="price"> Quantity <input type="text" name="product_quantity" placeholder="Enter quantity" required></p> 
             <p id="price"> Price <input type="text" name="product_price" placeholder="Enter price" required></p> 
+            <p id="price"> Paste URL to image to upload <input type="text" name ="post_img" placeholder="Enter URL"></p>
            
             <select name="post_type" id="post_type">
                 <option value="informative">Informative</option>
                 <option value="product">Product</option>
             </select>
             <!-- Images -->
-            <div id="images"><form action="../functions/upload.php" method="post" enctype="multipart/form-data">
+            <!-- <div id="images"><form action="../functions/upload.php" method="post" enctype="multipart/form-data">
                 Select image to upload
                 <input type="file" name="post_img" id="post_img">
-                <input type="submit" class = "post_button" name = "upload" value="Upload Post"/><br>
+                <input type="submit" class = "post_button" name = "upload" value="Upload Post"/><br> -->
             </form>
         </div>
         </form>  
