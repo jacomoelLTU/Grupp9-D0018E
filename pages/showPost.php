ca29c7c92e4cc82a6cf2bb4adfaad4d3e063a72f
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-24 16:01:20 +0100

    Fixing rollback

--- pages/showPost.php
+++ pages/showPost.php
@@ -27,9 +27,10 @@
 </form>
 
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//Add to isplay all errors
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
   mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
   include '../functions/config.php';
