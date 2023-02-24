d4b7461c5526feb26261059281924f7c6ffa4d1c
Adam <adalju-0@student.ltu.se>
2023-02-24 11:54:05 +0100

    msg

--- pages/printImage.php
+++ pages/printImage.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include 'config.php';
+include '../functions/config.php';
 $query = "SELECT post_title, post_img FROM post ORDER BY post_id DESC";
 $result = mysqli_query($conn, $query);
 
