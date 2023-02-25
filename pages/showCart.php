4182bab7bf28d5150857c81d93aff056c0a3caef
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-25 21:13:06 +0100

    Fixing rollback

--- pages/showCart.php
+++ pages/showCart.php
@@ -3,16 +3,14 @@ include '../functions/config.php';
 session_start();
 echo"Here is you items:";
 
+$ongoingtransactionid = $_SESSION['ongoingtransactionid'];
+$query = mysqli_query($conn, "SELECT product_id, product_title, product_price FROM product WHERE product_id='$ongoingtransactionid'");
 
-    foreach($_SESSION['objArr'] as $r){
-        $dataArr   = parse_str($r, $output);
-        $productId     = $output['productId'];
-        $productPrice  = $output['productPrice']; 
-        
-        echo "<div id='cartItem'>".$r.
+    while($row=mysqli_fetch_array($query, MYSQLI_ASSOC)){
+        echo "<div id='cartItem'>".$row['product_title'].
                 "<form method='post'>
                     <input type='submit' name='delObj' class='button' value='Del Item'/>
-                    <input type='hidden' name='obj' value=".$r.">
+                    <input type='hidden' name='item' value=".$row['product_id'].">
                 </form>
               </div><br>";   
     }
@@ -24,12 +22,6 @@ echo"Here is you items:";
     
     function delObj($conn) {
         
-        session_start();
-        $i = array_search($_POST['obj'], $_SESSION['objArr']);
-        if($_SESSION['objArr'][$i] == $_POST['obj']){            
-            unset($_SESSION['objArr'][$i]);
-            //mysqli_rollback($conn,1,$_POST['obj']);
-        }
     
     }
 ?>
