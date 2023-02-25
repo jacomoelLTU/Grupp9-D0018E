acfb0f19c87537b1ab47fc3d2e7b859fe985ab6b
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-25 21:14:16 +0100

    Fixing rollback

--- pages/showCart.php
+++ pages/showCart.php
@@ -1,5 +1,9 @@
 <?php
 include '../functions/config.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 echo"Here is you items:";
 
