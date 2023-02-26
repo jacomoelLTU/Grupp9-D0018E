f5f7a04609c7cdf0b22d2fd198f4a5b35510c0ef
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-26 10:01:53 +0100

    Fixing rollback

--- pages/showCart.php
+++ pages/showCart.php
@@ -5,31 +5,50 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_start();
-echo"Here is you items:";
-
-
-$ongoingtransactionid = $_SESSION['ongoingtransactionid'];
-$item = mysqli_query($conn, "SELECT transactionitem_productid FROM transactionitem WHERE transactionitem_transactionid='$ongoingtransactionid'");
-while($row=mysqli_fetch_array($item, MYSQLI_ASSOC)){
-    $currentProduct = $row['transactionitem_productid'];
-    $product = mysqli_query($conn, "SELECT product_id, product_title, product_price FROM product WHERE product_id='$currentProduct'");
-    while($row=mysqli_fetch_array($product, MYSQLI_ASSOC)){
-        echo "<div id='cartItem'>".$row['product_title'].
-                "<form method='post'>
-                    <input type='submit' name='delObj' class='button' value='Del Item'/>
-                    <input type='hidden' name='item' value=".$row['product_id'].">
-                </form>
-              </div><br>";   
-    }
-}
-    if(array_key_exists('delObj', $_POST)) {
-        delObj($conn);
-    }
+showItems($conn);
 
     //Functions ----------
-    
-    function delObj($conn) {
-        
-    
+function showItems($conn): void{
+    echo"Here is you items:";
+    try{
+    $ongoingtransactionid = $_SESSION['ongoingtransactionid'];
+    $item = mysqli_query($conn, "SELECT transactionitem_productid FROM transactionitem WHERE transactionitem_transactionid='$ongoingtransactionid'");
+    while($row=mysqli_fetch_array($item, MYSQLI_ASSOC)){
+        $currentProduct = $row['transactionitem_productid'];
+        $product = mysqli_query($conn, "SELECT product_id, product_title, product_price FROM product WHERE product_id='$currentProduct'");
+        while($row=mysqli_fetch_array($product, MYSQLI_ASSOC)){
+            echo "<div id='cartItem'>".$row['product_title'].
+                    "<form method='post'>
+                        <input type='submit' name='delObj' class='button' value='Del Item'/>
+                        <input type='hidden' name='item' value=".$row['product_id'].">
+                    </form>
+                    </div><br>";   
+            }
+            if(array_key_exists('delObj', $_POST)) {
+                delObj($conn);
+            }  
+        }
+    }catch(mysqli_sql_exception $e){
+        throw $e;
     }
+}
+
+function delObj($conn): void{
+    mysqli_begin_transaction($conn);
+    try{
+        $pid = $_POST['product_id'];
+        $query = mysqli_query($conn, "DELETE FROM transactionitem WHERE transactionitem_productid='$pid'");
+        mysqli_query($conn, $query);
+
+        
+        echo'<script>alert("Transaction started...");</script>';
+        mysqli_commit($conn);
+    }catch(mysqli_sql_exception $e){
+        echo'<script>alert("Rolling back...");</script>';
+        mysqli_rollback($conn);
+        throw $e;
+
+    }
+
+}
 ?>
