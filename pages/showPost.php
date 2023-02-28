7ce0182c046afda653e3f1482d5c69efd85a7cf7
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-28 22:06:40 +0100

    Fixing rollback

--- pages/showPost.php
+++ pages/showPost.php
@@ -1,33 +1,3 @@
-<link rel="stylesheet" type="text/css" href="../CSS/showPost.css">
-<a href="../index.php">To home</a>
-<center>
-<div id="postContainer">
-  <div id="grid-Title">Post Title:       <?php echo $_GET['postTitle'];       ?></div>
-  <div id="grid-Desc">Post Description: <?php echo $_GET['postDescription']; ?></div>
-  <div id="grid-Image">
-    <link rel="stylesheet" type="text/css" href="../CSS/printImage.css">
-    <div id="printImage">
-      <?php getImage($conn,  $postId);?>
-    </div>
-    <!-- För att lägga till alla bilder som är uppladdade på posten kan vi ha som whileloopen i php som printar ut img strängarna med rätt värden. -->
-    <!-- <div id="img-container">
-      <img src="../pictures/profilePictureTemplate.jpg" class="img1">
-      <img src="../pictures/img2.png" class="img2">
-    </div> -->
-  </div>
-  <div id="grid-B">B</div>
-  <div id="grid-C">C</div>
-  <div id="grid-D">D</div>
-  <div id="grid-E">E</div>
-  <div id="grid-F">F</div>
-  <div id="grid-G">G</div>
-</div>
-</center>
-
-<form method="post">
-  <input type="submit" name="insertToBasket" class="button" value="Add Item"/>
-</form>
-
 <?php
 //Add to isplay all errors
 // ini_set('display_errors', 1);
@@ -127,3 +97,33 @@ function getImage($conn, $postId): void{
   }
 }
 ?>
+
+<link rel="stylesheet" type="text/css" href="../CSS/showPost.css">
+<a href="../index.php">To home</a>
+<center>
+<div id="postContainer">
+  <div id="grid-Title">Post Title:       <?php echo $_GET['postTitle'];       ?></div>
+  <div id="grid-Desc">Post Description: <?php echo $_GET['postDescription']; ?></div>
+  <div id="grid-Image">
+    <link rel="stylesheet" type="text/css" href="../CSS/printImage.css">
+    <div id="printImage">
+      <?php getImage($conn,  $postId);?>
+    </div>
+    <!-- För att lägga till alla bilder som är uppladdade på posten kan vi ha som whileloopen i php som printar ut img strängarna med rätt värden. -->
+    <!-- <div id="img-container">
+      <img src="../pictures/profilePictureTemplate.jpg" class="img1">
+      <img src="../pictures/img2.png" class="img2">
+    </div> -->
+  </div>
+  <div id="grid-B">B</div>
+  <div id="grid-C">C</div>
+  <div id="grid-D">D</div>
+  <div id="grid-E">E</div>
+  <div id="grid-F">F</div>
+  <div id="grid-G">G</div>
+</div>
+</center>
+
+<form method="post">
+  <input type="submit" name="insertToBasket" class="button" value="Add Item"/>
+</form>
