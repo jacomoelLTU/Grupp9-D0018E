22e57f2120595ebdb1f284a2d795038ad7922dd5
wiklundemil <wiklund.emil2000@hotmail.com>
2023-02-28 22:03:46 +0100

    Fixing rollback

--- pages/showPost.php
+++ pages/showPost.php
@@ -105,6 +105,7 @@ function getImage($conn, $postId): void{
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
+  echo $postId;
   $query = "SELECT post_title, post_img FROM post WHERE post_id=$postId ";
   $result = mysqli_query($conn, $query);
 
