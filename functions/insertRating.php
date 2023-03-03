390672fd2d962ced2e195683426f946559e4f776
Adam <adalju-0@student.ltu.se>
2023-03-03 14:42:57 +0100

    moved updateAvgProductRating to insertRating

--- functions/insertRating.php
+++ functions/insertRating.php
@@ -20,9 +20,35 @@ if (isset($_POST["rating"])) {
         //query below is wrong cause you cant update a childs row, gonna look into this
         $insertQuery = "INSERT INTO rating(user_id, rating_productid, rating) VALUES ('" . $userId . "','" . $productId . "','" . $rating . "') ";
         $result = mysqli_query($conn, $insertQuery);
+        updateAvgProductRating();
         echo "success";
     } else {
         echo "Already Voted!";
     }
+}
+
+// This function updates the average rating of a product in the product table
+function updateAvgProductRating(){
+    include 'config.php';
+
+    session_start();
+    $userId = $_SESSION['userid'];
+    //$productId = $_GET['productid']; //this might be wrong way to get this, temp "solution" to move on
+    $productId = '35';
+
+    $query = "SELECT rating FROM rating WHERE rating_productid='$productId'";
+    $result = mysqli_query($conn, $query);
+
+    while($row = mysqli_fetch_array($result)){ //you might wanna use mysqli_fetch_array here or similar function, example: $result = mysqli_fetch_array($query, MYSQLI_ASSOC));
+        $iterations .= 1;
+        $totalRating .= $row;
+        echo $iterations . $totalRating;
+    }
+
+    //calculate average rating
+    $averageRating = $totalRating / $iterations;
+
+    $productRating = "UPDATE product SET product_rating=$averageRating WHERE product_id ='$productId'";
+    mysqli_query($conn, $productRating);
 }
 ?>
--- functions/updateAvgProductRating.php
+++ functions/updateAvgProductRating.php
@@ -1,26 +1,27 @@
 <!-- This function updates the average rating of a product in the product table -->
 <?php
-include 'config.php';
+function updateAvgProductRating(){
+    include 'config.php';
 
-session_start();
-$userId = $_SESSION['userid'];
-//$productId = $_GET['productid']; //this might be wrong way to get this, temp "solution" to move on
-$productId = '33';
+    session_start();
+    $userId = $_SESSION['userid'];
+    //$productId = $_GET['productid']; //this might be wrong way to get this, temp "solution" to move on
+    $productId = '35';
 
-$query = "SELECT rating FROM rating WHERE rating_productid='$productId'";
-$result = mysqli_query($conn, $query);
+    $query = "SELECT rating FROM rating WHERE rating_productid='$productId'";
+    $result = mysqli_query($conn, $query);
 
-while($row = mysqli_fetch_array($result)){ //you might wanna use mysqli_fetch_array here or similar function, example: $result = mysqli_fetch_array($query, MYSQLI_ASSOC));
-    $iterations .= 1;
-    $totalRating .= $row;
-    echo $iterations . $totalRating;
-    
+    while($row = mysqli_fetch_array($result)){ //you might wanna use mysqli_fetch_array here or similar function, example: $result = mysqli_fetch_array($query, MYSQLI_ASSOC));
+        $iterations .= 1;
+        $totalRating .= $row;
+        echo $iterations . $totalRating;
+        
+    }
+
+    //calculate average rating
+    $averageRating = $totalRating / $iterations;
+
+    $productRating = "UPDATE product SET product_rating=$averageRating WHERE product_id ='$productId'";
+    $insert = mysqli_query($conn, $productRating);
 }
-
-//calculate average rating
-$averageRating = $totalRating / $iterations;
-
-$productRating = "UPDATE product SET product_rating=$averageRating WHERE product_id ='$productId'";
-$insert = mysqli_query($conn, $productRating);
-
 ?>
