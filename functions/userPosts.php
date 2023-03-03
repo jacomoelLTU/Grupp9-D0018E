c3f09b67d9a3810670d6a42c2e91a500c429ef54
wiklundemil <wiklund.emil2000@hotmail.com>
2023-03-03 09:10:06 +0100

    Fixing edit page for idividuall users

--- functions/userPosts.php
+++ functions/userPosts.php
@@ -7,6 +7,10 @@ $userid = $_SESSION['userid'];
 
 $query = mysqli_query($conn, "SELECT * FROM post WHERE post_userid='$userid'");
 while($row=mysqli_fetch_array($query, MYSQLI_ASSOC)){
-    echo"Click to go to post ".$row['post_title'].": <a href ='showPost.php?postId=".$row['post_id']."&postTitle=".$row['post_title']."&postDescription=".$row['post_description']."'>Show post</a><br>";
+    echo"Click to go to post ".$row['post_title'].
+    ": <a href ='showPost.php?postId=".$row['post_id'].
+    "&postTitle=".$row['post_title']."&postDescription=".
+    $row['post_description']."'>Show post</a>
+    <a href='editPost?postId=".$row['post_id']."'><br>";
 }
 ?>
