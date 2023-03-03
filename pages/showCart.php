802e02d95b5f3dfdab65aeb51e5709854e0e945b
wiklundemil <wiklund.emil2000@hotmail.com>
2023-03-03 14:30:48 +0100

    Fixing edit page for idividuall users

--- pages/showCart.php
+++ pages/showCart.php
@@ -11,20 +11,21 @@ showItems($conn);
 function showItems($conn): void{
     echo"Here is you items:";
     try{
-        if(isset($_SESSION['ongoingtransactionid'])){
-    $ongoingtransactionid = $_SESSION['ongoingtransactionid'];
-    $item = mysqli_query($conn, "SELECT transactionitem_productid FROM transactionitem WHERE transactionitem_transactionid='$ongoingtransactionid'");
-    while($row=mysqli_fetch_array($item, MYSQLI_ASSOC)){
-        $currentProduct = $row['transactionitem_productid'];
-        $product = mysqli_query($conn, "SELECT product_id, product_title, product_price FROM product WHERE product_id='$currentProduct'");
-        while($row=mysqli_fetch_array($product, MYSQLI_ASSOC)){
-            echo "<div id='cartItem'>".$row['product_title'].$row['product_id'].
-                    "<form method='post'>
-                        <input type='submit' name='delObj' class='button' value='Del Item'/>
-                        <input type='hidden' name='item' value=".$row['product_id'].">
-                    </form>
-                    </div><br>";   
-            
+    session_start();
+    if(isset($_SESSION['ongoingtransactionid'])){
+        $ongoingtransactionid = $_SESSION['ongoingtransactionid'];
+        $item = mysqli_query($conn, "SELECT transactionitem_productid FROM transactionitem WHERE transactionitem_transactionid='$ongoingtransactionid'");
+        while($row=mysqli_fetch_array($item, MYSQLI_ASSOC)){
+            $currentProduct = $row['transactionitem_productid'];
+            $product = mysqli_query($conn, "SELECT product_id, product_title, product_price FROM product WHERE product_id='$currentProduct'");
+            while($row=mysqli_fetch_array($product, MYSQLI_ASSOC)){
+                echo "<div id='cartItem'>".$row['product_title'].$row['product_id'].
+                        "<form method='post'>
+                            <input type='submit' name='delObj' class='button' value='Del Item'/>
+                            <input type='hidden' name='item' value=".$row['product_id'].">
+                        </form>
+                        </div><br>";   
+                
                 if(array_key_exists('delObj', $_POST)) {
                     delObj($conn, $row['product_id']);
                 }     
