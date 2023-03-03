506cfa96fc7ea48c7265c7920eb4d70fb2f48881
wiklundemil <wiklund.emil2000@hotmail.com>
2023-03-03 12:54:44 +0100

    Fixing edit page for idividuall users

--- pages/editPost.php
+++ pages/editPost.php
@@ -24,6 +24,8 @@ function submitEdit($conn, $newTitle, $newDesc, $newPrice, $postId):void{
     mysqli_query($conn, "UPDATE product SET product_price=$newPrice WHERE product_postid=$postId");
    
     mysqli_commit($conn);
+    echo"<script>alert(SKer commit);</script>";
+
     }catch(mysqli_sql_exception $e){
     mysqli_rollback($conn);
     echo'<script>alert("Rolling back...");</script>';
@@ -34,6 +36,7 @@ function submitEdit($conn, $newTitle, $newDesc, $newPrice, $postId):void{
 //---------- Button SubmitEdit -------------
 if(array_key_exists('submitEdit', $_POST)) {
     if(isset($_GET['postId'])){
+        echo"<script>alert(klickade på submit knapp);</script>";
       submitEdit($conn, $_POST['updateTitle'], $_POST['updateDescription'], $_POST['updatePrice'], $_POST['post_id']);    
     }
   }
