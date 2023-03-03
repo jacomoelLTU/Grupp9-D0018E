e04de8a2a2986aeb47d9a02b1611b381501afb23
wiklundemil <wiklund.emil2000@hotmail.com>
2023-03-03 15:41:48 +0100

    Fixing edit page for idividuall users

--- pages/userPage.php
+++ pages/userPage.php
@@ -2,7 +2,7 @@
 <body>
     Click to go to <a href="../index.php">homepage</a>! 
     <div id="profileContainer">
-        <img style="width:100px; height:100px;" src="../pictures/profilePictureTemplate.jpg">
+        <img class="profilePicture" src="../pictures/profilePictureTemplate.jpg">
         <p>
         This is <?php session_start(); echo$_SESSION['username']." Med id: ".$_SESSION['userid'];?>'s page. 
         </p>
@@ -14,6 +14,6 @@
         }
     ?>
     <form method="post">
-        <input class="profilePicture" type="submit" name="submit", value="Submit">
+        <input type="submit" name="submit", value="Submit">
     </form>
 </body>
