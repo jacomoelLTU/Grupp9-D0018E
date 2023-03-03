45d6cc4360506596bc618acbcab4fe82f093b3bf
Adam <adalju-0@student.ltu.se>
2023-03-03 13:34:34 +0100

    revert revert

--- pages/examplePost.php
+++ pages/examplePost.php
@@ -17,6 +17,10 @@ Example how a post could look
     </div>
 </div> 
 
+<script type="text/javascript">
+    <?php include '../ratingFunctions.js'?> 
+</script>
+
 <body onload="getRating('../functions/getRatingData.php')">
     <div class="container">
         <h2>Rating System</h2>
@@ -25,7 +29,7 @@ Example how a post could look
 </body>
 
 <!-- Har detta script här medans jag testar -->
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 
     function getRating(url) {
         var xhttp = new XMLHttpRequest();
@@ -101,4 +105,4 @@ Example how a post could look
 
 
 
-
+ -->
