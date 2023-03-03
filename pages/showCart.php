a29dedce7837643d2cafad32f0b0c95bc8d551c0
wiklundemil <wiklund.emil2000@hotmail.com>
2023-03-03 14:26:57 +0100

    Fixing edit page for idividuall users

--- pages/showCart.php
+++ pages/showCart.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_start();
-if($_SESSION['ongoingtransactionid']){
+if(isset($_SESSION['ongoingtransactionid'])){
     showItems($conn);
 }
     //Functions ----------
