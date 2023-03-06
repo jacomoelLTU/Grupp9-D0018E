f87c8813e1211436850f06ef37da686627179b91
Adam <adalju-0@student.ltu.se>
2023-03-06 13:47:13 +0100

    gg

--- functions/updateAvgUserRating.php
+++ functions/updateAvgUserRating.php
@@ -12,7 +12,7 @@ function updateAvgUserRating($userId, $conn){
     $joinquery = mysqli_query($conn, "SELECT post.post_userid, post.post_id, product.product_rating FROM post INNER JOIN product ON post.post_id=product.product_postid;");
     
     while($row = mysqli_fetch_array($joinquery, MYSQLI_ASSOC)){
-        $outputstring .= "rowuserid:". $row['user_id'] . "userid: $userId";
+        $outputstring .= "rowuserid:". $row['post_userid'] . "userid: $userId";
         if($row['post_userid']==$userId && !empty($row['product_rating'])){
             $iterations += 1;
             $totalRating += $row['product_rating'];
