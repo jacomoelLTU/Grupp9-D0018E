307185f04c9d7720889527c8c5fa9463c48492cf
wiklundemil <wiklund.emil2000@hotmail.com>
2023-03-06 15:14:51 +0100

    Fixing edit page for idividuall users

--- functions/userPosts.php
+++ functions/userPosts.php
@@ -41,7 +41,7 @@ $userid = $_SESSION['userid'];
 
 $query = mysqli_query($conn, "SELECT * FROM post WHERE post_userid='$userid'");
 while($row=mysqli_fetch_array($query, MYSQLI_ASSOC)){
-    echo"<div id='postItem" .getImage($conn, $row['post_id'])."'>".$row['post_title'].
+    echo"<div id='postItem " .getImage($conn, $row['post_id'])."'>".$row['post_title'].
     ": <a href ='showPost.php?postId=".$row['post_id'].
     "&postTitle=".$row['post_title']."&postDescription=".
     $row['post_description']."'>Show post</a>
