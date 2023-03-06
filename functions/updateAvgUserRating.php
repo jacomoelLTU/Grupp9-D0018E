319a6b257a91a95cc2f8174e142d3b56c98cfd19
Adam <adalju-0@student.ltu.se>
2023-03-06 13:22:23 +0100

    very nice

--- functions/updateAvgUserRating.php
+++ functions/updateAvgUserRating.php
@@ -1,16 +1,17 @@
 <!-- This function updates the average rating for a user in the user table -->
 <?php
 function updateAvgUserRating($userId, $conn){
-    $query = "SELECT product_rating, product_postid FROM product WHERE product_userid='$userId'";
-    $result = mysqli_query($conn, $query);
+    //$query = "SELECT product_rating, product_postid FROM product WHERE product_userid='$userId'";
+    //$result = mysqli_query($conn, $query);
 
     $outputstring = "Outputstring: ";
     $iterations = 0;
     $totalRating = 0;
 
-    $join = mysqli_query($conn, "SELECT post.post_userid, post.post_id, product.product_rating FROM post WHERE post_userid=$userId INNER JOIN product ON post.post_id=product.product_postid;");
-    while($row = mysqli_fetch_array($join, MYSQLI_ASSOC)){
-        if($row['post_userid']==$userId){
+    $joinquery = mysqli_query($conn, "SELECT post.post_userid, post.post_id, product.product_rating FROM post WHERE post_userid=$userId INNER JOIN product ON post.post_id=product.product_postid;");
+    
+    while($row = mysqli_fetch_array($joinquery, MYSQLI_ASSOC)){
+        if($row['user_id']==$userId){
             $iterations += 1;
             $totalRating += $row['product_rating'];
             $outputstring .= "iteration: $iterations " . " totalrating: $totalRating";
@@ -20,8 +21,8 @@ function updateAvgUserRating($userId, $conn){
     //calculate average rating for user
     $averageRating = round($totalRating / $iterations);
 
-    $productRating = "UPDATE user SET user_rating=$averageRating WHERE user_id =$userId";
-    mysqli_query($conn, $productRating);
+    $userRating = "UPDATE user SET user_rating=$averageRating WHERE user_id =$userId";
+    mysqli_query($conn, $userRating);
     echo $outputstring;
 }
 ?>
