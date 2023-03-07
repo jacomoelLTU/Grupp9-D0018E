b23bc3268754fdc0c8b6fce40f38939cb64493c5
Adam <adalju-0@student.ltu.se>
2023-03-07 15:09:36 +0100

    msg

--- functions/posts.php
+++ functions/posts.php
@@ -17,26 +17,34 @@ require 'config.php';
     $product_price = $_POST['product_price'];
     $product_quantity = $_POST['product_quantity'];
 
-    //session for current user
-    session_start();
-    $uid = $_SESSION['userid'];
-    $postid = "SELECT post_id FROM post WHERE post_userid='$uid' AND post_title='$post_title'";
-    
-    //query insert post
-    $sql_post = "INSERT INTO post (post_userid, post_title, post_description, post_img, post_type) VALUES ('$uid', '$post_title', '$post_description', '$post_img', '$post_type')";
+    try{
+        mysqli_begin_transaction($conn);
+        //session for current user
+        session_start();
+        $uid = $_SESSION['userid'];
+        $postid = "SELECT post_id FROM post WHERE post_userid='$uid' AND post_title='$post_title'";
+        
+        //query insert post
+        $sql_post = "INSERT INTO post (post_userid, post_title, post_description, post_img, post_type) VALUES ('$uid', '$post_title', '$post_description', '$post_img', '$post_type')";
 
-    //lock tables so posts cant have duplicate product_postid in the product table
-    mysqli_query($conn, "LOCK TABLES product WRITE, post WRITE");
+        //lock tables so posts cant have duplicate product_postid in the product table
+        mysqli_query($conn, "LOCK TABLES product WRITE, post WRITE");
 
-    //insert into mysql
-    $rs_post = mysqli_query($conn, $sql_post);
+        //insert into mysql
+        $rs_post = mysqli_query($conn, $sql_post);
 
-    //lägger till insert till product table med det nyss tillagda POST ID vi har
-    $query_lastPostid = mysqli_query($conn,"SELECT MAX(post_id) AS maximum FROM post");  //satement gets the just added postID
-    $maxID = $query_lastPostid->fetch_array()[0] ?? '';
-    $sql_queryProduct = "INSERT INTO product (product_postid, product_title, product_price, product_quantity) VALUES ($maxID, '$post_title', $product_price, $product_quantity)";
-    $product_insert = mysqli_query($conn, $sql_queryProduct);
-    mysqli_query($conn, "UNLOCK TABLES");
-
+        //lägger till insert till product table med det nyss tillagda POST ID vi har
+        $query_lastPostid = mysqli_query($conn,"SELECT MAX(post_id) AS maximum FROM post");  //satement gets the just added postID
+        $maxID = $query_lastPostid->fetch_array()[0] ?? '';
+        $sql_queryProduct = "INSERT INTO product (product_postid, product_title, product_price, product_quantity) VALUES ($maxID, '$post_title', $product_price, $product_quantity)";
+        $product_insert = mysqli_query($conn, $sql_queryProduct);
+        mysqli_query($conn, "UNLOCK TABLES");
+        mysqli_commit($conn);
+    }
+    catch(mysqli_sql_exception $e){
+        mysqli_rollback($conn);
+        echo'<script>alert("Rolling back...");</script>';
+        throw $e;
+    }
     header('Location: ../index.php');
 ?>
