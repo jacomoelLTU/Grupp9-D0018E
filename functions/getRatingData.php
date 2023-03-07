99f4cbdbf60143fcbbfaf38dbee762d0fa3fa1bd
Adam <adalju-0@student.ltu.se>
2023-03-07 10:28:15 +0100

    msg

--- functions/getRatingData.php
+++ functions/getRatingData.php
@@ -1,7 +1,6 @@
 <?php
 require_once "ratingFunctions.php";
 require_once "config.php";
-include "../pages/showPost.php";
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -10,9 +9,11 @@ error_reporting(E_ALL);
 echo "Hello world!<br>";
 
 session_start();
+$postId = $GLOBALS['postId'];
 $userId = $_SESSION['userid'];
 //$postId = $_GET['postId'];
-$postId = '1244';
+//$postId = '1244';
+//$postId = getPostId();
 
 //get product id from product table
 $productIdquery = mysqli_query($conn, "SELECT product_id FROM product WHERE product_postid=$postId;");
