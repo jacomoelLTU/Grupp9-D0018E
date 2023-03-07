08bcad2a653d56c377b769810e8b11a223e3fe16
Adam <adalju-0@student.ltu.se>
2023-03-07 10:52:34 +0100

    msg

--- functions/getRatingData.php
+++ functions/getRatingData.php
@@ -11,9 +11,7 @@ echo "Hello world!<br>";
 session_start();
 $userId = $_SESSION['userid'];
 //$postId = $_GET['postId'];
-//$postId = '1244';
-'auto_prepend_file = "../pages/showPost.php"';
-$postId = getPostId();
+$postId = '1244';
 
 //get product id from product table
 $productIdquery = mysqli_query($conn, "SELECT product_id FROM product WHERE product_postid=$postId;");
