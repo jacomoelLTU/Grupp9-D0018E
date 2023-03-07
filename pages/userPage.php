386f3026958477a60bc43354063fab20396ce07a
wiklundemil <wiklund.emil2000@hotmail.com>
2023-03-07 10:31:47 +0100

    Fixing edit page for idividuall users

--- pages/userPage.php
+++ pages/userPage.php
@@ -1,5 +1,8 @@
 <link rel="stylesheet" type="text/css" href="../CSS/userPage.css">
 <body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<button type="submit" class="btn btn-primary"><span class="fa fa-search"></span> Search</button>
+<button type="submit" class="btn btn-secondary"><span class="fa fa-search"></span> Search</button>
     Click to go to <a href="../index.php">homepage</a>! 
     <div id="profileContainer">
         <img class="profilePicture" src="../pictures/profilePictureTemplate.jpg">
