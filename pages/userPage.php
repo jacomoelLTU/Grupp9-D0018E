3a04a9bc38873b5d8e0e6bf1e034a3b896fb581e
Adam <adalju-0@student.ltu.se>
2023-03-07 14:53:17 +0100

    msg

--- pages/userPage.php
+++ pages/userPage.php
@@ -1,12 +1,17 @@
 <link rel="stylesheet" type="text/css" href="../CSS/userPage.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 <body>
-
+<?php
+session_start();
+$userId = $_SESSION['userid'];
+$userRating = mysqli_query($conn, "SELECT user_rating FROM user WHERE user_id=$userId;");
+$row = mysqli_fetch_array($userRating, MYSQLI_ASSOC);
+?>
     Click to go to <a href="../index.php"><i class="bi bi-house-door"></i></a>! 
     <div id="profileContainer">
         <img class="profilePicture" src="../pictures/profilePictureTemplate.jpg">
         <p>
-        This is <?php session_start(); echo$_SESSION['username']." Med id: ".$_SESSION['userid'];?>'s page. 
+        This is <?php echo$_SESSION['username']." Med id: ".$_SESSION['userid']." Med rating: ".$row['user_rating'];?>'s page. 
         </p>
         <center>
             <div id="postContainer">
