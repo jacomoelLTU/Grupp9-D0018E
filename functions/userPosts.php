edb67d5cdbde611f1160fd8821036da7c4ab3d86
wiklundemil <wiklund.emil2000@hotmail.com>
2023-03-07 13:38:10 +0100

    Fixing edit page for idividuall users

--- functions/userPosts.php
+++ functions/userPosts.php
@@ -6,7 +6,7 @@ include 'config.php';
 //Requier = import library but not include i.e print out. content.
 require 'showPost.php';
 
-function getImage($conn, $postId): string{
+function getImageItem($conn, $postId): string{
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
@@ -43,7 +43,7 @@ $userid = $_SESSION['userid'];
 $query = mysqli_query($conn, "SELECT * FROM post WHERE post_userid='$userid'");
 while($row=mysqli_fetch_array($query, MYSQLI_ASSOC)){
     echo"
-    <div id='postItem' " .getImage($conn, $row['post_id'])."'> 
+    <div id='postItem' " .getImageItem($conn, $row['post_id'])."'> 
         <a class='showItemText'  href ='showPost.php?postId=".$row['post_id']."&postTitle=".$row['post_title']."&postDescription=".$row['post_description']."'></div>
         <a class='addItemIcon' href='".insertToBasket($conn, $postId)."'><i class='bi bi-bag-plus'></i></a>
         <a class='editItemIcon' href='../pages/editPost.php?postId=".$row['post_id']."'><i class='bi bi-three-dots-vertical'></i></a>
