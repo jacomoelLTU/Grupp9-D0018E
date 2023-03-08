9caba6c9045fbb17f2d016a12858ad1cf74a1d55
Adam <adalju-0@student.ltu.se>
2023-03-08 15:04:18 +0100

    userposts

--- functions/userPosts.php
+++ functions/userPosts.php
@@ -27,7 +27,7 @@ function getImage($conn, $postId): string{
             return "style='background-image:url(data:image/jpeg;base64,".$image."');";
         }
         else {
-          return "style='background-image:url(data:image/jpeg;base64, '../pictures/No-image-Placeholder.png');";
+          return "";
         }
     }
     return "";
