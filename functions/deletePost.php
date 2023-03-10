423d69a192a87b6a99a1f06e403a3ac67d8763ea
Adam <adalju-0@student.ltu.se>
2023-03-10 12:17:16 +0100

    admintest

--- functions/deletePost.php
+++ functions/deletePost.php
@@ -7,6 +7,18 @@ error_reporting(E_ALL);
 require 'config.php';
 
 $postId = $_POST['post_id'];
-$query = mysqli_query($conn,"DELETE FROM post WHERE post_id='$postId'");
+
+//get product id to be able to delete from rating table
+$productIdQuery = mysqli_query($conn,"SELECT product_id FROM product WHERE product_postid='$postId'");
+$productIdRow = mysqli_fetch_array($query, MYSQLI_ASSOC);
+
+//delete rating
+$ratingQuery = mysqli_query($conn,"DELETE FROM rating WHERE rating_productid='$productIdRow[product_id]");
+
+//delete product
+$productQuery = mysqli_query($conn,"DELETE FROM product WHERE product_postid='$postId'");
+
+//delete post
+$postQuery = mysqli_query($conn,"DELETE FROM post WHERE post_id='$postId'");
 
 ?>
