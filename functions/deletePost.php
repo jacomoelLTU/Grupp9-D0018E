f1de2c5ee373b168b9a6520b34fdd68180693b3f
Adam <adalju-0@student.ltu.se>
2023-03-13 16:16:19 +0100

    gg

--- functions/deletePost.php
+++ functions/deletePost.php
@@ -1,9 +1,6 @@
 <!-- DELETE POST action to mysql -->
 
 <?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 require 'config.php';
 
 $postId = $_POST['post_id'];
