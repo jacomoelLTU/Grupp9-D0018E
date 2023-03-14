9039879cc95e77c00ace010559a721cde24195c8
Adam <adalju-0@student.ltu.se>
2023-03-14 11:25:32 +0100

    gg

--- pages/signUpForm.php
+++ pages/signUpForm.php
@@ -21,7 +21,7 @@
             <h1 class ="SignUpHeader"> Provide user information to register </h1>
             <input type = "text" class = "SignUp-input" name = "userName" placeholder = "Username"/> 
             <input type = "text" class = "SignUp-input" name = "passWord" placeholder = "Password"/>
-            <input type = "text" class = "SignUp-input"  name = "userEmailAdress" placeholder = "Email Adress"/>
+            <input type = "text" class = "SignUp-input"  name = "userEmailAdress" placeholder = "Email Address"/>
             <input type = "text" class = "SignUp-input" name = "userFirstname" placeholder = "First Name"/>
             <input type = "text" class = "SinUp-input" name = "userSurname" placeholder = "Surname"/>
 
