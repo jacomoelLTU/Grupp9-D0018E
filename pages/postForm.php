b5cbfbb8eea89dfe3fb42d0193b41f710d00f0b4
Adam <adalju-0@student.ltu.se>
2023-03-14 11:46:00 +0100

    maxlength for post

--- pages/postForm.php
+++ pages/postForm.php
@@ -11,8 +11,8 @@ Click to go to <a href="../index.php">homepage</a>!
         
         <!-- Text Fields -->
         <form action="../functions/posts.php" method="post">
-            <p id="title"> Title <br><input type="text" name="post_title" placeholder="Enter title" required></p> 
-            <p id="description"> Description <br><br><textarea rows = "5" cols = "40" name = "post_description" placeholder="Enter description" required></textarea></p>
+            <p id="title"> Title <br><input type="text" name="post_title" placeholder="Enter title" maxlength="40" required ></p> 
+            <p id="description"> Description <br><br><textarea rows = "5" cols = "40" name = "post_description" placeholder="Enter description" maxlength="200" required></textarea></p>
             <p id="price"> Quantity <br><input type="number" min=0 name="product_quantity" placeholder="Enter quantity" required></p> 
             <p id="price"> Price <br><input type="number" min=0 name="product_price" placeholder="Enter price" required></p> 
             <p id="price"> Paste URL to image <br><input type="text" name ="post_img" placeholder="Enter URL"></p>
